cde3bf411299035cc0490835166a3c4f3743da8e
Alexander Lobakin <alobakin@mailbox.org>
2025-02-04 19:20:56 +0100

    visual: allow shader override basing on texture name
    In addition to
    
    [vis_shaders_replace]
    monsters = models\model,models\model_pn
    
    you can now do something like
    
    [vis_shaders_override]
    act = models\model,models\model_pn
    corp = models\model,models\model_pn
    
    or
    
    [vis_shaders_override]
    act\act_arm_perchatka = models\model,models\model_pn
    
    etc.
    The line above demonstrates why this might be needed: SoC has weapon
    and hands in one _hud.ogf, where both hands and weapon can be
    models\model, but we want to enable tesselation for hands only.
    
    Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

--- ogsr_engine/Layers/xrRender/FBasicVisual.cpp
+++ ogsr_engine/Layers/xrRender/FBasicVisual.cpp
@@ -4,12 +4,10 @@
 
 #include "stdafx.h"
 
-
 #include "../../xr_3da/render.h"
 
 #include "fbasicvisual.h"
 #include "../../xr_3da/fmesh.h"
-
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
@@ -32,15 +30,17 @@ dxRender_Visual::~dxRender_Visual() {}
 
 void dxRender_Visual::Release() {}
 
+constexpr const char* vis_shaders_replace = "vis_shaders_replace";
+constexpr const char* vis_shaders_override = "vis_shaders_override";
 
 static bool replaceShadersLine(const char* N, char* fnS, u32 fnS_size, LPCSTR item)
 {
-    if (!pSettings->line_exist("vis_shaders_replace", item))
+    if (!pSettings->line_exist(vis_shaders_replace, item))
         return false;
 
-    LPCSTR overrides = pSettings->r_string("vis_shaders_replace", item);
+    LPCSTR overrides = pSettings->r_string(vis_shaders_replace, item);
     u32 cnt = _GetItemCount(overrides);
-    ASSERT_FMT(cnt % 2 == 0, "[%s]: vis_shaders_replace: wrong format cnt = %u: %s = %s", __FUNCTION__, cnt, item, overrides);
+    ASSERT_FMT(cnt % 2 == 0, "[%s]: %s: wrong format cnt = %u: %s = %s", __FUNCTION__, vis_shaders_replace, cnt, item, overrides);
 
     for (u32 i = 0; i < cnt; i += 2)
     {
@@ -50,7 +50,7 @@ static bool replaceShadersLine(const char* N, char* fnS, u32 fnS_size, LPCSTR it
         if (xr_strcmp(s1, fnS) == 0)
         {
             xr_strcpy(fnS, fnS_size, s2);
-            //Msg("~~[%s][%s] replaced [%s] by [%s]", __FUNCTION__, N, s1, s2);
+            // Msg("~~[%s][%s] replaced [%s] by [%s]", __FUNCTION__, N, s1, s2);
             break;
         }
     }
@@ -60,7 +60,7 @@ static bool replaceShadersLine(const char* N, char* fnS, u32 fnS_size, LPCSTR it
 
 static bool replaceShaders(const char* N, char* fnS, u32 fnS_size)
 {
-    if (!pSettings->section_exist("vis_shaders_replace"))
+    if (!pSettings->section_exist(vis_shaders_replace))
         return false;
 
     if (replaceShadersLine(N, fnS, fnS_size, N))
@@ -84,6 +84,56 @@ static bool replaceShaders(const char* N, char* fnS, u32 fnS_size)
     return false;
 }
 
+static bool overrideShadersLine(const char* N, char* fnS, u32 fnS_size, const char* item)
+{
+    if (!pSettings->line_exist(vis_shaders_override, item))
+        return false;
+
+    LPCSTR overrides = pSettings->r_string(vis_shaders_override, item);
+    u32 cnt = _GetItemCount(overrides);
+    ASSERT_FMT(cnt % 2 == 0, "[%s]: %s: wrong format cnt = %u: %s = %s", __FUNCTION__, vis_shaders_override, cnt, item, overrides);
+
+    for (u32 i = 0; i < cnt; i += 2)
+    {
+        string256 s1, s2;
+        _GetItem(overrides, i, s1);
+        _GetItem(overrides, i + 1, s2);
+        if (xr_strcmp(s1, fnS) == 0)
+        {
+            xr_strcpy(fnS, fnS_size, s2);
+            // Msg("~~[%s][%s] overridden [%s] by [%s]", __FUNCTION__, N, s1, s2);
+            break;
+        }
+    }
+
+    return true;
+}
+
+static bool overrideShaders(const char* fnT, char* fnS, u32 fnS_size)
+{
+    if (!pSettings->section_exist(vis_shaders_override))
+        return false;
+
+    if (overrideShadersLine(fnT, fnS, fnS_size, fnT))
+        return true;
+
+    std::string s{fnT};
+
+    if (strchr(fnT, ':'))
+    {
+        s.erase(s.find(":"));
+        if (overrideShadersLine(fnT, fnS, fnS_size, s.c_str()))
+            return true;
+    }
+
+    while (xr_string_utils::SplitFilename(s))
+    {
+        if (overrideShadersLine(fnT, fnS, fnS_size, s.c_str()))
+            return true;
+    }
+
+    return false;
+}
 
 void dxRender_Visual::Load(const char* N, IReader* data, u32)
 {
@@ -115,12 +165,16 @@ void dxRender_Visual::Load(const char* N, IReader* data, u32)
         string256 fnT, fnS;
         data->r_stringZ(fnT, sizeof(fnT));
         data->r_stringZ(fnS, sizeof(fnS));
-        if (replaceShaders(N, fnS, sizeof fnS)) {
-            //Msg("~~[%s] replaced shaders for [%s]: %s", __FUNCTION__, N, fnS);
+        if (replaceShaders(N, fnS, sizeof fnS))
+        {
+            // Msg("~~[%s] replaced shaders for [%s]: %s", __FUNCTION__, N, fnS);
+        }
+        if (overrideShaders(fnT, fnS, sizeof(fnS)))
+        {
+            // Msg("~~[%s] overridden shaders for [%s]: %s", __FUNCTION__, N, fnS);
         }
         shader.create(fnS, fnT);
     }
-
 }
 
 #define PCOPY(a) a = pFrom->a
