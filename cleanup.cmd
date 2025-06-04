68b00d682f99b1ff5a69506062f73f74755c2eee
Alexander Lobakin <alobakin@mailbox.org>
2025-06-04 16:31:02 +0200

    build: purge x32 build configurations
    Stop pretending we support it.
    
    Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

--- cleanup.cmd
+++ cleanup.cmd
@@ -19,7 +19,6 @@ RD /s /q 3rd_party\Src\xxHash\_Build
 RD /s /q 3rd_party\Src\zstd\_Build
 RD /s /q ogsr_engine\_LIB
 RD /s /q ogsr_engine\_TEMP
-RD /s /q bin_x86\
 RD /s /q bin_x64\
 for /d %%B in (.vs\Engine\*) do (
 	del %%B\*.VC.*
