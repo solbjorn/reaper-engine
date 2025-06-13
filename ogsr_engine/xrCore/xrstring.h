solbjorn/reaper-engine

commit 4261fe4de9ea6359b69523e8a309c5e772de29fa

xrCore: replace {xr,string}_unordered_map with absl::flat_hash_map
It already support heterogenous lookups, uses abls::Hash and performs
better in most cases.
Despite that this required to pull a good bunch of libs from Abseil,
the size of exe increased by only 3 Kb, which means it's even more
optimized than std::unordered_map.

Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrCore/xrstring.h
@@ -1,6 +1,7 @@
 #ifndef xrstringH
 #define xrstringH
 
+#include <absl/container/flat_hash_map.h>
 #include <absl/hash/hash.h>
 
 //////////////////////////////////////////////////////////////////////////
@@ -190,29 +191,8 @@ IC void xr_strlwr(shared_str& src)
     }
 }
 
-struct transparent_string_hash
-{
-    using is_transparent = void; // https://www.cppstories.com/2021/heterogeneous-access-cpp20/
-    using hash_type = absl::Hash<absl::string_view>;
-    using is_avalanching = void;
-
-    [[nodiscard]] auto operator()(const absl::string_view txt) const noexcept -> u64 { return hash_type{}(txt); }
-    [[nodiscard]] auto operator()(const std::string& txt) const noexcept -> u64 { return hash_type{}(txt); }
-    [[nodiscard]] auto operator()(const char* txt) const noexcept -> u64 { return hash_type{}(txt); }
-    [[nodiscard]] auto operator()(const shared_str& txt) const noexcept -> u64 { return hash_type{}(txt); }
-};
-
-struct transparent_string_equal
-{
-    using is_transparent = void;
-
-    [[nodiscard]] bool operator()(const absl::string_view lhs, const absl::string_view rhs) const { return lhs == rhs; }
-    [[nodiscard]] bool operator()(const shared_str& lhs, const shared_str& rhs) const { return lhs == rhs; }
-    [[nodiscard]] bool operator()(const char* lhs, const char* rhs) const { return !strcmp(lhs, rhs); }
-};
-
-template <typename Key, typename Value, class _Alloc = xr_allocator<std::pair<const Key, Value>>>
-using string_unordered_map = std::unordered_map<Key, Value, transparent_string_hash, transparent_string_equal, _Alloc>;
+template <class K, class V, class Hash = absl::DefaultHashContainerHash<K>, class Eq = absl::DefaultHashContainerEq<K>, class Allocator = xr_allocator<std::pair<const K, V>>>
+using string_unordered_map = absl::flat_hash_map<K, V, Hash, Eq, Allocator>;
 
 namespace xr_string_utils
 {

3rd_party/Src/abseil/abseil.vcxproj
@@ -70,12 +70,21 @@
       cmake --build . --config $(Configuration)
       md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base
       copy /Y "absl\base\absl_base.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_base.lib"
+      copy /Y "absl\base\absl_malloc_internal.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_malloc_internal.lib"
       copy /Y "absl\base\absl_raw_logging_internal.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_raw_logging_internal.lib"
       copy /Y "absl\base\absl_spinlock_wait.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_spinlock_wait.lib"
+      copy /Y "absl\base\absl_throw_delegate.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_throw_delegate.lib"
+      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\container
+      copy /Y "absl\container\absl_hashtablez_sampler.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\container\absl_hashtablez_sampler.lib"
+      copy /Y "absl\container\absl_raw_hash_set.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\container\absl_raw_hash_set.lib"
+      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\debugging
+      copy /Y "absl\debugging\absl_stacktrace.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\debugging\absl_stacktrace.lib"
       md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash
       copy /Y "absl\hash\absl_city.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash\absl_city.lib"
       copy /Y "absl\hash\absl_hash.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash\absl_hash.lib"
       copy /Y "absl\hash\absl_low_level_hash.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash\absl_low_level_hash.lib"
+      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\numeric
+      copy /Y "absl\numeric\absl_int128.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\numeric\absl_int128.lib"
       md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random
       copy /Y "absl\random\absl_random_distributions.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_distributions.lib"
       copy /Y "absl\random\absl_random_internal_entropy_pool.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_entropy_pool.lib"
@@ -89,35 +98,16 @@
       copy /Y "absl\random\absl_random_seed_sequences.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_seed_sequences.lib"
       md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\strings
       copy /Y "absl\strings\absl_string_view.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\strings\absl_string_view.lib"
+      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\synchronization
+      copy /Y "absl\synchronization\absl_kernel_timeout_internal.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\synchronization\absl_kernel_timeout_internal.lib"
+      copy /Y "absl\synchronization\absl_synchronization.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\synchronization\absl_synchronization.lib"
+      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\time
+      copy /Y "absl\time\absl_time.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\time\absl_time.lib"
+      copy /Y "absl\time\absl_time_zone.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\time\absl_time_zone.lib"
     </NMakeBuildCommandLine>
     <NMakeReBuildCommandLine>
-      set "PATH=%PATH%;%VSAPPIDDIR%\CommonExtensions\Microsoft\CMake\CMake\bin;%VSAPPIDDIR%\CommonExtensions\Microsoft\CMake\Ninja"
       RD /S/Q _Build
-      md _Build
-      cd _Build
-      cmake -G Ninja -DCMAKE_BUILD_TYPE=$(Configuration) -DCMAKE_INSTALL_PREFIX="$(Configuration)\$(PlatformShortName)" -DCMAKE_TOOLCHAIN_FILE=..\..\3rd_party_toolchain.cmake -DCMAKE_PROJECT_INCLUDE="$(SolutionDir)\3rd_party\Src\3rd_party_properties.cmake" ..\abseil-cpp
-      cmake --build . --config $(Configuration)
-      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base
-      copy /Y "absl\base\absl_base.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_base.lib"
-      copy /Y "absl\base\absl_raw_logging_internal.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_raw_logging_internal.lib"
-      copy /Y "absl\base\absl_spinlock_wait.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\base\absl_spinlock_wait.lib"
-      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash
-      copy /Y "absl\hash\absl_city.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash\absl_city.lib"
-      copy /Y "absl\hash\absl_hash.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash\absl_hash.lib"
-      copy /Y "absl\hash\absl_low_level_hash.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\hash\absl_low_level_hash.lib"
-      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random
-      copy /Y "absl\random\absl_random_distributions.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_distributions.lib"
-      copy /Y "absl\random\absl_random_internal_entropy_pool.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_entropy_pool.lib"
-      copy /Y "absl\random\absl_random_internal_platform.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_platform.lib"
-      copy /Y "absl\random\absl_random_internal_randen.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_randen.lib"
-      copy /Y "absl\random\absl_random_internal_randen_hwaes.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_randen_hwaes.lib"
-      copy /Y "absl\random\absl_random_internal_randen_hwaes_impl.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_randen_hwaes_impl.lib"
-      copy /Y "absl\random\absl_random_internal_randen_slow.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_randen_slow.lib"
-      copy /Y "absl\random\absl_random_internal_seed_material.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_internal_seed_material.lib"
-      copy /Y "absl\random\absl_random_seed_gen_exception.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_seed_gen_exception.lib"
-      copy /Y "absl\random\absl_random_seed_sequences.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\random\absl_random_seed_sequences.lib"
-      md ..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\strings
-      copy /Y "absl\strings\absl_string_view.lib" "..\..\..\_LIB\$(Configuration)\$(PlatformShortName)\absl\strings\absl_string_view.lib"
+      $(NMakeBuildCommandLine)
     </NMakeReBuildCommandLine>
     <NMakeCleanCommandLine>RD /S/Q _Build</NMakeCleanCommandLine>
   </PropertyGroup>

ogsr_engine/xrCore/_stl_extensions.h
@@ -2,6 +2,7 @@
 
 #include <queue>
 
+#include <absl/container/flat_hash_map.h>
 #include <absl/hash/hash.h>
 
 #include "xalloc.h"
@@ -48,8 +49,8 @@ using xr_map = std::map<K, V, P, allocator>;
 template <typename K, class V, class P = std::less<K>, typename allocator = xr_allocator<std::pair<const K, V>>>
 using xr_multimap = std::multimap<K, V, P, allocator>;
 
-template <typename K, typename V, class _Hasher = absl::Hash<K>, class _Keyeq = std::equal_to<K>, class _Alloc = xr_allocator<std::pair<const K, V>>>
-using xr_unordered_map = std::unordered_map<K, V, _Hasher, _Keyeq, _Alloc>;
+template <class K, class V, class Hash = absl::DefaultHashContainerHash<K>, class Eq = absl::DefaultHashContainerEq<K>, class Allocator = xr_allocator<std::pair<const K, V>>>
+using xr_unordered_map = absl::flat_hash_map<K, V, Hash, Eq, Allocator>;
 
 #define mk_pair std::make_pair // TODO: Везде заменить, а это убрать.
 

ogsr_engine/xr_3da/XR_3DA.vcxproj
@@ -24,11 +24,16 @@
   <Import Project="$(VCTargetsPath)\Microsoft.Cpp.Default.props" />
   <Import Project="..\Engine_properties.props" />
   <PropertyGroup Label="UserMacros">
-    <AbseilBase>absl\base\absl_base.lib;absl\base\absl_raw_logging_internal.lib;absl\base\absl_spinlock_wait.lib</AbseilBase>
+    <AbseilBase>absl\base\absl_base.lib;absl\base\absl_malloc_internal.lib;absl\base\absl_raw_logging_internal.lib;absl\base\absl_spinlock_wait.lib;absl\base\absl_throw_delegate.lib</AbseilBase>
+    <AbseilContainer>absl\container\absl_hashtablez_sampler.lib;absl\container\absl_raw_hash_set.lib</AbseilContainer>
+    <AbseilDebugging>absl\debugging\absl_stacktrace.lib</AbseilDebugging>
     <AbseilHash>absl\hash\absl_city.lib;absl\hash\absl_hash.lib;absl\hash\absl_low_level_hash.lib</AbseilHash>
+    <AbseilNumeric>absl\numeric\absl_int128.lib</AbseilNumeric>
     <AbseilRandom>absl\random\absl_random_distributions.lib;absl\random\absl_random_internal_entropy_pool.lib;absl\random\absl_random_internal_platform.lib;absl\random\absl_random_internal_randen.lib;absl\random\absl_random_internal_randen_hwaes.lib;absl\random\absl_random_internal_randen_hwaes_impl.lib;absl\random\absl_random_internal_randen_slow.lib;absl\random\absl_random_internal_seed_material.lib;absl\random\absl_random_seed_gen_exception.lib;absl\random\absl_random_seed_sequences.lib</AbseilRandom>
     <AbseilStrings>absl\strings\absl_string_view.lib</AbseilStrings>
-    <AbseilLibs>$(AbseilBase);$(AbseilHash);$(AbseilRandom);$(AbseilStrings)</AbseilLibs>
+    <AbseilSynchronization>absl\synchronization\absl_kernel_timeout_internal.lib;absl\synchronization\absl_synchronization.lib</AbseilSynchronization>
+    <AbseilTime>absl\time\absl_time.lib;absl\time\absl_time_zone.lib</AbseilTime>
+    <AbseilLibs>$(AbseilBase);$(AbseilContainer);$(AbseilDebugging);$(AbseilHash);$(AbseilNumeric);$(AbseilRandom);$(AbseilStrings);$(AbseilSynchronization);$(AbseilTime)</AbseilLibs>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'" Label="Configuration">
     <ConfigurationType>Application</ConfigurationType>

commit 8087227f6f3c330893f5d1ca16de77cce0372eb2

xrCore: use absl::string_view for string_unordered_map hash and eq
Force absl::string_view type for hash and comparison functions,
otherwise heterogenous lookup doesn't always work if the key is
a shared_str. string_view is universal and every string type
can be represented by it.
-5-8 Kb of exe size.

Fixes: 4261fe4de9ea ("xrCore: replace {xr,string}_unordered_map with absl::flat_hash_map")
Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrCore/xrstring.h
@@ -191,7 +191,7 @@ IC void xr_strlwr(shared_str& src)
     }
 }
 
-template <class K, class V, class Hash = absl::DefaultHashContainerHash<K>, class Eq = absl::DefaultHashContainerEq<K>, class Allocator = xr_allocator<std::pair<const K, V>>>
+template <class K, class V, class Hash = absl::DefaultHashContainerHash<absl::string_view>, class Eq = absl::DefaultHashContainerEq<absl::string_view>, class Allocator = xr_allocator<std::pair<const K, V>>>
 using string_unordered_map = absl::flat_hash_map<K, V, Hash, Eq, Allocator>;
 
 namespace xr_string_utils