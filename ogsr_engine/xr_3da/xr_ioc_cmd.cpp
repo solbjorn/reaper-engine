solbjorn/reaper-engine

commit 518cd76da2989e71a1fb3d141bc5c2d3aef31d9d

xrSound: port true sound occlusion by materials and env from dsh
The one that I took from OXR doesn't work well and causes the same sound
play very loud or very quiet without any reasons.
Now I see that @dsh2dsh has an excellent implementation of sound
occlusion based on many different factors.
Pick this one and clean the code up where applicable.

Unfortunately, I was observing sound corruptions when playing caused by
the Doppler Effects, so I'm defaulting it to 0 (off) for now. Maybe
later I'll take a look and fix it.

Also install default alsoft.ini and HRTF configs to bin_x64/.

Co-developed-by: Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
Signed-off-by: Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
Co-developed-by: JR <5493488+joye-ramone@users.noreply.github.com>
Signed-off-by: JR <5493488+joye-ramone@users.noreply.github.com>
Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xr_3da/xr_ioc_cmd.cpp
@@ -730,11 +730,17 @@ void CCC_Register()
     CMD2(CCC_Float, "snd_volume_eff", &psSoundVEffects);
     CMD2(CCC_Float, "snd_volume_music", &psSoundVMusic);
     CMD1(CCC_SND_Restart, "snd_restart");
-    // CMD3(CCC_Mask, "snd_acceleration", &psSoundFlags, ss_Hardware);
+
     CMD3(CCC_Mask, "snd_efx", &psSoundFlags, ss_EFX);
     CMD3(CCC_Mask, "snd_use_float32", &psSoundFlags, ss_UseFloat32);
     CMD4(CCC_Integer, "snd_targets", &psSoundTargets, 128, 1024);
-    CMD4(CCC_Float, "snd_linear_fade", &psSoundLinearFadeFactor, 0.1f, 1.f);
+
+    CMD4(CCC_Float, "snd_rolloff", &psSoundRolloff, 0.1f, 2.f);
+    CMD4(CCC_Float, "snd_fade_speed", &psSoundFadeSpeed, 1.f, 10.f);
+    CMD4(CCC_Float, "snd_occ_scale", &psSoundOcclusionScale, 0.1f, 1.f);
+    CMD4(CCC_Float, "snd_occ_hf", &psSoundOcclusionHf, 0.f, 1.f);
+    CMD4(CCC_Float, "snd_occ_mtl", &psSoundOcclusionMtl, 0.f, 1.f);
+
     // Doppler effect power
     CMD4(CCC_Float, "snd_doppler_power", &soundSmoothingParams::power, 0.f, 5.f);
     CMD4(CCC_SoundParamsSmoothing, "snd_doppler_smoothing", &soundSmoothingParams::steps, 1, 100);

3rd_party/Src/openal-soft/configs/alsoft.ini
@@ -1,0 +1,10 @@
+[general]
+channels	= stereo
+stereo-mode	= headphones
+stereo-encoding	= hrtf
+hrtf		= true
+hrtf-mode	= full
+default-hrtf	= CIAIR_48000
+
+[decoder]
+hq-mode		= true

3rd_party/Src/openal-soft/openal-soft.vcxproj
@@ -51,6 +51,8 @@
       md "$(SolutionDir)\bin_$(PlatformShortName)"
       copy /Y "OpenAL32.dll" "$(SolutionDir)\bin_$(PlatformShortName)\OpenAL32.dll"
       copy /Y "OpenAL32.pdb" "$(SolutionDir)\bin_$(PlatformShortName)\OpenAL32.pdb"
+      copy /Y "..\configs\alsoft.ini" "$(SolutionDir)\bin_$(PlatformShortName)\alsoft.ini"
+      copy /Y "..\configs\CIAIR_48000.mhr" "$(SolutionDir)\bin_$(PlatformShortName)\CIAIR_48000.mhr"
     </NMakeBuildCommandLine>
     <NMakeReBuildCommandLine>
       set "PATH=%PATH%;%VSAPPIDDIR%\CommonExtensions\Microsoft\CMake\CMake\bin;%VSAPPIDDIR%\CommonExtensions\Microsoft\CMake\Ninja"
@@ -64,6 +66,8 @@
       md "$(SolutionDir)\bin_$(PlatformShortName)"
       copy /Y "OpenAL32.dll" "$(SolutionDir)\bin_$(PlatformShortName)\OpenAL32.dll"
       copy /Y "OpenAL32.pdb" "$(SolutionDir)\bin_$(PlatformShortName)\OpenAL32.pdb"
+      copy /Y "..\configs\alsoft.ini" "$(SolutionDir)\bin_$(PlatformShortName)\alsoft.ini"
+      copy /Y "..\configs\CIAIR_48000.mhr" "$(SolutionDir)\bin_$(PlatformShortName)\CIAIR_48000.mhr"
     </NMakeReBuildCommandLine>
     <NMakeCleanCommandLine>RD /S/Q _Build</NMakeCleanCommandLine>
   </PropertyGroup>

ogsr_engine/xrCDB/xr_area.cpp
@@ -12,7 +12,7 @@ using namespace collide;
 
 extern BOOL g_bLoaded;
 
-void IGame_Level::SoundEvent_Register(ref_sound_data_ptr S, float range)
+void IGame_Level::SoundEvent_Register(ref_sound_data_ptr S, float range, float time_to_stop)
 {
     if (!g_bLoaded)
         return;
@@ -74,7 +74,7 @@ void IGame_Level::SoundEvent_Register(ref_sound_data_ptr S, float range)
             Power *= occ;
             if (Power > EPS_S)
             {
-                _esound_delegate D = {L, S, Power};
+                _esound_delegate D = {.dest = L, .source = S, .power = Power, .time_to_stop = time_to_stop};
                 snd_Events.push_back(D);
             }
         }
@@ -91,7 +91,7 @@ void IGame_Level::SoundEvent_Dispatch()
         if (D.source->feedback)
         {
             D.dest->feel_sound_new(D.source->g_object, D.source->g_type, D.source->g_userdata,
-                                   D.source->feedback->is_2D() ? Device.vCameraPosition : D.source->feedback->get_params()->position, D.power);
+                                   D.source->feedback->is_2D() ? Device.vCameraPosition : D.source->feedback->get_params()->position, D.power, D.time_to_stop);
         }
         snd_Events.pop_back();
     }
@@ -103,10 +103,10 @@ void IGame_Level::SoundEvent_OnDestDestroy(Feel::Sound* obj)
     snd_Events.erase(std::remove_if(snd_Events.begin(), snd_Events.end(), [obj](const _esound_delegate& d) { return d.dest == obj; }), snd_Events.end());
 }
 
-void __stdcall _sound_event(ref_sound_data_ptr S, float range)
+void __stdcall _sound_event(ref_sound_data_ptr S, float range, float time_to_stop)
 {
     if (g_pGameLevel && S && S->feedback)
-        g_pGameLevel->SoundEvent_Register(S, range);
+        g_pGameLevel->SoundEvent_Register(S, range, time_to_stop);
 }
 
 //----------------------------------------------------------------------
@@ -126,8 +126,10 @@ CObjectSpace::CObjectSpace()
 //----------------------------------------------------------------------
 CObjectSpace::~CObjectSpace()
 {
-    Sound->set_geometry_occ(NULL);
-    Sound->set_handler(NULL);
+    Sound->set_geometry_occ(nullptr);
+    Sound->set_geometry_som(nullptr);
+    Sound->set_geometry_env(nullptr);
+    Sound->set_handler(nullptr);
 #ifdef DEBUG
     sh_debug.destroy();
 #endif

ogsr_engine/xrGame/Actor.h
@@ -122,7 +122,7 @@ public:
     // Render
     void renderable_Render(u32 context_id, IRenderable* root) override;
     virtual BOOL renderable_ShadowGenerate();
-    virtual void feel_sound_new(CObject* who, int type, CSound_UserDataPtr user_data, const Fvector& Position, float power);
+    virtual void feel_sound_new(CObject* who, int type, CSound_UserDataPtr user_data, const Fvector& Position, float power, float time_to_stop);
     virtual Feel::Sound* dcast_FeelSound() { return this; }
     float m_snd_noise;
 #ifdef DEBUG

ogsr_engine/xrGame/Actor_Feel.cpp
@@ -82,7 +82,7 @@ ICF static BOOL info_trace_callback(collide::rq_result& result, LPVOID params)
     }
     else
     {
-        //получить треугольник и узнать его материал
+        // получить треугольник и узнать его материал
         CDB::TRI* T = Level().ObjectSpace.GetStaticTris() + result.element;
         const auto* mtl = GMLib.GetMaterialByIdx(T->material);
         if (mtl->Flags.is(SGameMtl::flPassable))
@@ -152,11 +152,8 @@ void CActor::PickupModeUpdate_COD()
         return;
     }
 
-    //подбирание объекта
-    if (inventory().m_pTarget 
-        && inventory().m_pTarget->Useful() 
-        && m_pUsableObject 
-        && m_pUsableObject->nonscript_usable() &&
+    // подбирание объекта
+    if (inventory().m_pTarget && inventory().m_pTarget->Useful() && m_pUsableObject && m_pUsableObject->nonscript_usable() &&
         !Level().m_feel_deny.is_object_denied(smart_cast<CGameObject*>(inventory().m_pTarget)))
     {
         CInventoryItem* pNearestItem = inventory().m_pTarget;
@@ -323,7 +320,7 @@ void CActor::PickupInfoDraw(CObject* object)
     HUD().Font().pFontLetterica16Russian->Out(x, y, draw_str);
 }
 
-void CActor::feel_sound_new(CObject* who, int type, CSound_UserDataPtr user_data, const Fvector& Position, float power)
+void CActor::feel_sound_new(CObject* who, int type, CSound_UserDataPtr user_data, const Fvector& Position, float power, float time_to_stop)
 {
     if (who == this)
         m_snd_noise = _max(m_snd_noise, power);

ogsr_engine/xrGame/CustomMonster.cpp
@@ -794,11 +794,11 @@ bool CCustomMonster::is_special_killer(CObject* obj) { return (obj && (std::find
 
 float CCustomMonster::feel_vision_mtl_transp(CObject* O, u32 element) { return (memory().visual().feel_vision_mtl_transp(O, element)); }
 
-void CCustomMonster::feel_sound_new(CObject* who, int type, CSound_UserDataPtr user_data, const Fvector& position, float power)
+void CCustomMonster::feel_sound_new(CObject* who, int type, CSound_UserDataPtr user_data, const Fvector& position, float power, float time_to_stop)
 {
     if (getDestroy())
         return;
-    memory().sound().feel_sound_new(who, type, user_data, position, power);
+    memory().sound().feel_sound_new(who, type, user_data, position, power, time_to_stop);
 }
 
 bool CCustomMonster::useful(const CItemManager* manager, const CGameObject* object) const { return (memory().item().useful(object)); }

commit 1e2637776666b9c94561ae7910f25edb12756f6b

xrSound: remove occlusion override leftovers
Lol I didn't know this gets read from system.ltx/[sound], overwriting
the value from user.ltx...

Fixes: 518cd76da298 ("xrSound: port true sound occlusion by materials and env from dsh")
Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xr_3da/xr_ioc_cmd.cpp
@@ -768,11 +768,6 @@ void CCC_Register()
     CMD1(CCC_r2, "renderer");
 
     CMD1(CCC_soundDevice, "snd_device");
-    // CMD3(CCC_Mask, "snd_device_default", &psSoundFlags, ss_UseDefaultDevice);
-
-    // psSoundRolloff	= pSettings->r_float	("sound","rolloff");		clamp(psSoundRolloff,			EPS_S,	2.f);
-    psSoundOcclusionScale = pSettings->r_float("sound", "occlusion_scale");
-    clamp(psSoundOcclusionScale, 0.1f, .5f);
 
 #ifdef DEBUG
     CMD1(CCC_DumpOpenFiles, "dump_open_files");