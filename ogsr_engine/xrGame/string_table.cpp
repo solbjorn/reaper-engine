solbjorn/reaper-engine

commit 2a5834474b19a341f7c8f01fabcef083dc522706

xrGame: load string tables in parallel
From OXR. They reverted it for now due to some issues on Linux, but
reaper-engine is Windows-exclusive (at least for now), so let's try.
I haven't observed any issues so far. Will see when I get back from
my vanilla test root to ReapeR build, where there are way more string
tables to load.

Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrGame/string_table.cpp
@@ -1,10 +1,14 @@
 #include "stdafx.h"
-#include "string_table.h"
 
+#include <oneapi/tbb/parallel_for_each.h>
+
+#include "string_table.h"
 #include "ui/xrUIXmlParser.h"
 #include "xr_level_controller.h"
 
+std::mutex CStringTable::pDataMutex;
 STRING_TABLE_DATA* CStringTable::pData{};
+
 BOOL CStringTable::WriteErrorsToLog{};
 
 CStringTable::CStringTable() { Init(); }
@@ -26,20 +30,23 @@ void CStringTable::Init()
     LPCSTR S = pSettings->r_string("string_table", "files");
     if (S && S[0])
     {
-        string128 xml_file;
-        int count = _GetItemCount(S);
-        for (int it = 0; it < count; ++it)
-        {
-            _GetItem(S, it, xml_file);
-            Load(xml_file);
-        }
+        size_t count = _GetItemCount(S);
+
+        oneapi::tbb::parallel_for(oneapi::tbb::blocked_range<size_t>(0, count), [this, &S](const auto& range) {
+            for (size_t i = range.begin(); i != range.end(); i++)
+            {
+                string128 xml_file;
+
+                _GetItem(S, i, xml_file);
+                Load(xml_file);
+            }
+        });
     }
 
     if (pSettings->section_exist("string_table_files"))
     {
         CInifile::Sect& files = pSettings->r_section("string_table_files");
-        for (const auto& i : files.Ordered_Data)
-            Load(i.first.c_str());
+        oneapi::tbb::parallel_for_each(files.Ordered_Data, [&](const auto& i) { Load(i.first.c_str()); });
     }
 }
 
@@ -56,22 +63,23 @@ void CStringTable::Load(LPCSTR xml_file)
         Debug.fatal(DEBUG_INFO, "string table xml file not found %s, for language %s", xml_file_full, *(pData->m_sLanguage));
 
     // общий список всех записей таблицы в файле
-    const int string_num = uiXml.GetNodesNum(uiXml.GetRoot(), "string");
+    const size_t string_num = uiXml.GetNodesNum(uiXml.GetRoot(), "string");
 
-    for (int i = 0; i < string_num; ++i)
+    for (size_t i = 0; i < string_num; i++)
     {
         LPCSTR string_name = uiXml.ReadAttrib(uiXml.GetRoot(), "string", i, "id", NULL);
-
-        if (WriteErrorsToLog && pData->m_StringTable.contains(string_name))
-            Msg("!![%s] duplicate string table id: [%s]", __FUNCTION__, string_name);
-
         LPCSTR string_text = uiXml.Read(uiXml.GetRoot(), "string:text", i, NULL);
-
         VERIFY3(string_text, "string table entry does not has a text", string_name);
 
-        STRING_VALUE str_val = ParseLine(string_text, string_name, true);
+        const STRING_VALUE str_val = ParseLine(string_text, string_name, true);
+        {
+            std::lock_guard guard{pDataMutex};
 
-        pData->m_StringTable[string_name] = str_val;
+            if (WriteErrorsToLog && pData->m_StringTable.contains(string_name))
+                Msg("!![%s] duplicate string table id: [%s]", __FUNCTION__, string_name);
+
+            pData->m_StringTable[string_name] = str_val;
+        }
     }
 }
 

ogsr_engine/xrGame/string_table.h
@@ -35,5 +35,6 @@ private:
     void Load(LPCSTR xml_file);
 
     static STRING_VALUE ParseLine(LPCSTR str, LPCSTR key, bool bFirst);
+    static std::mutex pDataMutex;
     static STRING_TABLE_DATA* pData;
 };

commit 2bd0b1da4dd62de5b14b6be58d6840966e6ef959

xrGame: fix extremely rare crash when loading string tables
Half a year passed since the tagged commit and I caught this crash for
the first time ever...
Anyway, ParseLine() is not thread-safe since it can insert entries to
the keybinding map (to update string tables when rebinding a key).
It's enough to wrap this insertion with the same mutex that protects
string tables themselves, as this happens before we take it to insert
a string.

Fixes: 2a5834474b19 ("xrGame: load string tables in parallel")
Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrGame/string_table.cpp
@@ -94,15 +94,13 @@ void CStringTable::Load(LPCSTR xml_file)
         LPCSTR string_text = uiXml.Read(uiXml.GetRoot(), node, i, nullptr);
         ASSERT_FMT(string_text, "no attribute '%s' in node %s", node, string_name);
 
-        const STRING_VALUE str_val = ParseLine(string_text, string_name, true);
-        {
-            std::lock_guard guard{pDataMutex};
+        const auto str_val{ParseLine(string_text, string_name, true)};
+        std::scoped_lock lock{pDataMutex};
 
-            if (WriteErrorsToLog && pData->m_StringTable.contains(string_name))
-                Msg("!![%s] duplicate string table id: [%s]", __FUNCTION__, string_name);
+        if (WriteErrorsToLog && pData->m_StringTable.contains(string_name))
+            Msg("!![%s] duplicate string table id: [%s]", __FUNCTION__, string_name);
 
-            pData->m_StringTable[string_name] = str_val;
-        }
+        pData->m_StringTable[string_name] = str_val;
     }
 }
 
@@ -112,7 +110,11 @@ void CStringTable::ReparseKeyBindings()
         return;
 
     for (const auto& [key, val] : pData->m_string_key_binding)
-        pData->m_StringTable[key] = ParseLine(val.c_str(), key.c_str(), false);
+    {
+        const auto str{ParseLine(val.c_str(), key.c_str(), false)};
+        std::scoped_lock lock{pDataMutex};
+        pData->m_StringTable[key] = str;
+    }
 }
 
 STRING_VALUE CStringTable::ParseLine(LPCSTR str, LPCSTR skey, bool bFirst)
@@ -157,7 +159,10 @@ STRING_VALUE CStringTable::ParseLine(LPCSTR str, LPCSTR skey, bool bFirst)
     }
 
     if (b_hit && bFirst)
+    {
+        std::scoped_lock lock{pDataMutex};
         pData->m_string_key_binding[skey] = str;
+    }
 
     return STRING_VALUE(res.c_str());
 }