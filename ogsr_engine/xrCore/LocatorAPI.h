solbjorn/reaper-engine

commit e5d9683ac7f3d7d126a47c0e9900030d385f3bd2

xrCore: add support for SquashFS archives (lol)
DB is a suboptimal format for several reasons:
* per-file compression
* LZO compression format
* no streaming for compressed files

LZO is old, weak (esp. per-file), and utilizes CPU.

After looking at several different formats like 7z etc., the easiest
thing to me was to take libsquashfs from squashfs-tools-ng, which has
support for Windows, and use it to open and read SquashFS archives,
compressed with either LZ4 (max speed) or Zstd (balanced, still better
in both than LZO).

Advantages:
* deduplicates files and blocks
* block-based, you can stream even from a compressed archive
* supports all new fancy compression formats
* you can browse a .sq[uash]fs file in newer 7-Zip (and create them
  using squashfs-tool[-ng])
* you can compress files of different formats in several archives with
  different block sizes, compressors etc. to achieve best perf and
  smallest size, and all that will work

Impl. details:

LocationAPI now doesn't have any format-specific methods, only wrappers
to call the appropriate one. arc_db.cpp and arc_sqfs.cpp contain those.
I don't use libsquashfs' streams and define my own on top of
CStreamReader, with the stream buffer size equal to the block size of
the underlying FS). When I need to read a full file, I read it directly.

libsquashfs is not thread-safe by default. To read a file from the
archive, you need thread-local copies of 3 objects: sqfs_file_t,
sqfs_dir_reader_t, and sqfs_data_reader_t, because they are stateful.
To not allocate and initialize them each time you need to read a file,
I just use lockless MPMC queue from Cameron314 to recycle those objects
when a thread finished reading and doesn't need them anymore. This works
well for both cases when an app uses a fixed number of worker threads
and when the task manager dynamically spawns and terminates new threads
(e.g. like in oneTBB).

Vanilla gamedata.db0-dbd was 5.7 Gb in original, now with LZ4-HC

ogsr_engine/xrCore/LocatorAPI.h
@@ -11,9 +11,11 @@
 
 #include "LocatorAPI_defs.h"
 
-constexpr size_t VFS_STANDARD_FILE = std::numeric_limits<size_t>::max();
-
 class CStreamReader;
+
+struct sqfs_compressor_t;
+struct sqfs_dir_iterator_t;
+struct sqfs_file_t;
 
 class CLocatorAPI
 {
@@ -22,33 +24,110 @@ class CLocatorAPI
 public:
     struct file
     {
-        LPCSTR name; // low-case name
+        const char* name; // low-case name
         size_t vfs; // VFS_STANDARD_FILE - standart file
-        u32 ptr; // pointer inside vfs
-        u32 size_real; //
-        u32 size_compressed; // if (size_real==size_compressed) - uncompressed
+
+        u32 size_real;
+        u32 folder : 1;
+
+        union
+        {
+            // SquashFS
+            u64 inode;
+
+            // DB
+            struct
+            {
+                u32 ptr; // pointer inside vfs
+                u32 size_compressed; // if (size_real==size_compressed) - uncompressed
+            };
+        };
+
         u32 modif; // for editor
-        bool folder{};
     };
 
 private:
+    static constexpr size_t VFS_STANDARD_FILE = std::numeric_limits<size_t>::max();
+    static constexpr size_t BIG_FILE_READER_WINDOW_SIZE = 1024 * 1024;
+
     struct file_pred
     {
         IC bool operator()(const file& x, const file& y) const { return xr_strcmp(x.name, y.name) < 0; }
     };
+
     struct archive
     {
         shared_str path;
-        void *hSrcFile, *hSrcMap;
-        CInifile* header;
         size_t vfs_idx;
         size_t size;
-        u32 key;
 
-        archive() : hSrcFile(NULL), hSrcMap(NULL), header(NULL), vfs_idx(VFS_STANDARD_FILE), size(0), key(0) {}
+    private:
+        class xr_sqfs_stream;
+        struct xr_sqfs;
 
+        enum container
+        {
+            STANDARD,
+            SQFS,
+            DB,
+        };
+        container type : 3;
+
+        union
+        {
+            // SquashFS
+            struct
+            {
+                sqfs_file_t* file;
+                sqfs_compressor_t* cmp;
+                xr_sqfs* fs;
+            };
+
+            // DB
+            struct
+            {
+                void* hSrcMap;
+                CInifile* header;
+                u32 key;
+            };
+        };
+
+        void* hSrcFile;
+
+    public:
+        archive() : hSrcFile(NULL), vfs_idx(VFS_STANDARD_FILE), size(0), type(container::STANDARD), file(nullptr), cmp(nullptr), fs(nullptr) {}
+
+        // Implementation wrappers
         void open();
+        IC bool autoload();
+        IC const char* entry_point();
+        IC void index(CLocatorAPI& loc, const char* fs_entry_point);
+        IC IReader* read(const char* fname, const struct file& desc, u32 gran);
+        IC CStreamReader* stream(const char* fname, const struct file& desc);
+        IC void cleanup();
         void close();
+
+    private:
+        // SquashFS
+        void open_sqfs();
+        bool autoload_sqfs();
+        const char* entry_point_sqfs();
+        void index_dir_sqfs(CLocatorAPI& loc, const char* path, sqfs_dir_iterator_t* it);
+        void index_sqfs(CLocatorAPI& loc, const char* fs_entry_point);
+        IReader* read_sqfs(const char* fname, const struct file& desc, u32 gran);
+        CStreamReader* stream_sqfs(const char* fname, const struct file& desc);
+        void cleanup_sqfs();
+        void close_sqfs();
+
+        // DB
+        void open_db();
+        bool autoload_db();
+        const char* entry_point_db();
+        void index_db(CLocatorAPI& loc, const char* entry_point);
+        IReader* read_db(const char* fname, const struct file& desc, u32 gran);
+        CStreamReader* stream_db(const char* fname, const struct file& desc);
+        void cleanup_db();
+        void close_db();
     };
 
     DEFINE_MAP_PRED(LPCSTR, FS_Path*, PathMap, PathPairIt, pred_str);

ogsr_engine/xrCore/arc_sqfs.cpp
@@ -1,0 +1,352 @@
+#include "stdafx.h"
+
+#include <concurrentqueue.h>
+
+#include <sqfs/compressor.h>
+#include <sqfs/data_reader.h>
+#include <sqfs/dir_entry.h>
+#include <sqfs/dir_reader.h>
+#include <sqfs/id_table.h>
+#include <sqfs/inode.h>
+#include <sqfs/io.h>
+
+#include "FS_internal.h"
+#include "stream_reader.h"
+
+struct CLocatorAPI::archive::xr_sqfs
+{
+    struct reader
+    {
+        sqfs_file_t* file;
+        sqfs_dir_reader_t* dr;
+        sqfs_data_reader_t* data;
+    };
+
+private:
+    moodycamel::ConcurrentQueue<reader*> q;
+
+    reader* alloc(const archive& arc);
+
+public:
+    sqfs_super_t super{};
+
+    reader* get(const archive& arc)
+    {
+        reader* rd;
+
+        if (!q.try_dequeue(rd))
+            rd = alloc(arc);
+
+        return rd;
+    }
+
+    bool put(reader* rd) { return q.enqueue(rd); }
+
+    void drain();
+};
+
+CLocatorAPI::archive::xr_sqfs::reader* CLocatorAPI::archive::xr_sqfs::alloc(const archive& arc)
+{
+    reader* rd = xr_new<reader>();
+
+    R_ASSERT(!sqfs_file_open(&rd->file, *arc.path, SQFS_FILE_OPEN_READ_ONLY));
+
+    rd->dr = sqfs_dir_reader_create(&arc.fs->super, arc.cmp, rd->file, 0);
+    rd->data = sqfs_data_reader_create(rd->file, arc.fs->super.block_size, arc.cmp, 0);
+
+    R_ASSERT(rd->dr && rd->data);
+    R_ASSERT(!sqfs_data_reader_load_fragment_table(rd->data, &arc.fs->super));
+
+    return rd;
+}
+
+void CLocatorAPI::archive::xr_sqfs::drain()
+{
+    reader* rd;
+
+    while (q.try_dequeue(rd))
+    {
+        sqfs_drop(rd->data);
+        sqfs_drop(rd->dr);
+        sqfs_drop(rd->file);
+
+        xr_delete(rd);
+    }
+}
+
+/* Stream reader for a file inside squashfs */
+
+class CLocatorAPI::archive::xr_sqfs_stream : public CStreamReader
+{
+private:
+    const archive* arc{};
+    xr_sqfs::reader* rd;
+    sqfs_inode_generic_t* inode;
+
+    size_t fbase{};
+    size_t fsize{};
+    size_t foff{};
+
+    size_t bsize{};
+    size_t bread{};
+    size_t boff{};
+
+    u8* buf;
+    u64 ref{};
+    size_t wnd{};
+
+    void destroy()
+    {
+        if (!arc)
+            return;
+
+        xr_free(buf);
+        xr_free(inode);
+        arc->fs->put(rd);
+
+        memset(reinterpret_cast<u8*>(this) + offsetof(xr_sqfs_stream, arc), 0, sizeof(*this) - offsetof(xr_sqfs_stream, arc));
+    }
+
+public:
+    void construct(const archive* arcin, u64 inodein, size_t base, size_t sz, size_t wndin);
+
+    void construct(const archive* arcin, u64 inodein, size_t sz, size_t wndin) { construct(arcin, inodein, 0, sz, wndin); }
+
+    intptr_t elapsed() const override { return fsize - tell(); }
+    const size_t& length() const override { return fsize; }
+    size_t tell() const override { return foff + boff; }
+    void seek(const int& offset) override { advance(offset - tell()); }
+
+    void close() override
+    {
+        destroy();
+
+        xr_sqfs_stream* self = this;
+        xr_delete(self);
+    }
+
+    void advance(const int& offset) override
+    {
+        s64 noff = s64(boff) + offset;
+        if (noff >= 0 && size_t(noff) < bsize)
+        {
+            boff = noff;
+            s64 nread = s64(bread) - offset;
+            bread = std::max<s64>(nread, 0);
+
+            if (offset < 0)
+                R_ASSERT(sqfs_data_reader_read(rd->data, inode, fbase + foff + boff, buf + boff, -offset) == -offset);
+        }
+        else
+        {
+            foff += noff;
+
+            size_t woff = rounddown(foff, wnd);
+            boff = foff - woff;
+            foff = woff;
+
+            bread = 0;
+        }
+    }
+
+    void r(void* buffer, size_t buffer_size) override;
+    CStreamReader* open_chunk(const size_t& chunk_id) override;
+};
+
+void CLocatorAPI::archive::xr_sqfs_stream::construct(const CLocatorAPI::archive* arcin, u64 inodein, size_t base, size_t sz, size_t wndin)
+{
+    if (arc)
+        destroy();
+
+    arc = arcin;
+    fbase = base;
+    fsize = sz;
+
+    ref = inodein;
+    wnd = wndin;
+    R_ASSERT(std::has_single_bit(wnd));
+
+    rd = arc->fs->get(*arc);
+    R_ASSERT(!sqfs_dir_reader_get_inode(rd->dr, ref, &inode));
+
+    bsize = std::min(wnd, fsize);
+    buf = xr_alloc<u8>(bsize);
+}
+
+void CLocatorAPI::archive::xr_sqfs_stream::r(void* buffer, size_t buffer_size)
+{
+    u8* cbuffer = reinterpret_cast<u8*>(buffer);
+
+    while (buffer_size && !eof())
+    {
+        if (!bread)
+        {
+            size_t precache = std::min<s64>(bsize - boff, elapsed());
+
+            R_ASSERT(sqfs_data_reader_read(rd->data, inode, fbase + foff + boff, buf + boff, precache) == (sqfs_s32)precache);
+            bread += precache;
+        }
+
+        size_t copy = std::min(buffer_size, bread);
+        memcpy(cbuffer, buf + boff, copy);
+
+        cbuffer += copy;
+        buffer_size -= copy;
+
+        int off = (int)copy;
+        advance(off);
+    }
+}
+
+CStreamReader* CLocatorAPI::archive::xr_sqfs_stream::open_chunk(const size_t& chunk_id)
+{
+    BOOL compressed;
+
+    const auto size = find_chunk(chunk_id, &compressed);
+    if (!size)
+        return nullptr;
+
+    R_ASSERT2(!compressed, "cannot use xr_sqfs_stream on compressed chunks");
+
+    xr_sqfs_stream* st = xr_new<xr_sqfs_stream>();
+    st->construct(arc, ref, tell(), size, wnd);
+
+    return st;
+}
+
+/* SquashFS */
+
+void CLocatorAPI::archive::open_sqfs()
+{
+    type = container::SQFS;
+    fs = xr_new<xr_sqfs>();
+
+    R_ASSERT(!sqfs_file_open(&file, *path, SQFS_FILE_OPEN_READ_ONLY));
+    R_ASSERT(!sqfs_super_read(&fs->super, file));
+
+    sqfs_compressor_config_t cfg;
+    sqfs_compressor_config_init(&cfg, (SQFS_COMPRESSOR)fs->super.compression_id, fs->super.block_size, SQFS_COMP_FLAG_UNCOMPRESS);
+
+    R_ASSERT(!sqfs_compressor_create(&cfg, &cmp));
+}
+
+bool CLocatorAPI::archive::autoload_sqfs()
+{
+    // SquashFS doesn't have metadata, always autoload archives
+    return true;
+}
+
+const char* CLocatorAPI::archive::entry_point_sqfs()
+{
+    // Same as above: no meta -> default entry point
+    return nullptr;
+}
+
+void CLocatorAPI::archive::index_dir_sqfs(CLocatorAPI& loc, const char* path, sqfs_dir_iterator_t* it)
+{
+    sqfs_dir_entry_t* ent;
+
+    for (;;)
+    {
+        if (it->next(it, &ent))
+            break;
+
+        string256 full;
+        strconcat(sizeof(full), full, path, ent->name);
+
+        if ((ent->mode & SQFS_INODE_MODE_MASK) == SQFS_INODE_MODE_DIR)
+        {
+            strconcat(sizeof(full), full, full, "\\");
+
+            sqfs_dir_iterator_t* sub;
+
+            it->open_subdir(it, &sub);
+            index_dir_sqfs(loc, full, sub);
+            sqfs_drop(sub);
+        }
+        else
+        {
+            union
+            {
+                u64 inode;
+                struct
+                {
+                    u32 ptr;
+                    u32 size_compressed;
+                };
+            } attr = {
+                .inode = ent->inode,
+            };
+
+            loc.Register(full, vfs_idx, attr.ptr, ent->size, attr.size_compressed, 0);
+        }
+
+        xr_free(ent);
+    }
+}
+
+void CLocatorAPI::archive::index_sqfs(CLocatorAPI& loc, const char* fs_entry_point)
+{
+    sqfs_inode_generic_t* inode;
+    sqfs_dir_iterator_t* it;
+    sqfs_id_table_t* idt;
+
+    idt = sqfs_id_table_create(0);
+    R_ASSERT(idt);
+    R_ASSERT(!sqfs_id_table_read(idt, file, &fs->super, cmp));
+
+    auto* rd = fs->get(*this);
+    R_ASSERT(!sqfs_dir_reader_get_root_inode(rd->dr, &inode));
+
+    R_ASSERT(!sqfs_dir_iterator_create(rd->dr, idt, rd->data, nullptr, inode, &it));
+    xr_free(inode);
+
+    index_dir_sqfs(loc, fs_entry_point, it);
+    sqfs_drop(it);
+
+    sqfs_drop(idt);
+    fs->put(rd);
+}
+
+IReader* CLocatorAPI::archive::read_sqfs(const char* fname, const struct file& desc, u32 gran)
+{
+    sqfs_inode_generic_t* inode;
+    auto* rd = fs->get(*this);
+
+    if (sqfs_dir_reader_get_inode(rd->dr, desc.inode, &inode))
+        return nullptr;
+
+    u8* dest = xr_alloc<u8>(desc.size_real);
+    R_ASSERT(sqfs_data_reader_read(rd->data, inode, 0, dest, desc.size_real) == (s32)desc.size_real);
+
+    xr_free(inode);
+    fs->put(rd);
+
+    return xr_new<CTempReader>(dest, desc.size_real, 0);
+}
+
+CStreamReader* CLocatorAPI::archive::stream_sqfs(const char* fname, const struct file& desc)
+{
+    xr_sqfs_stream* st = xr_new<xr_sqfs_stream>();
+    st->construct(this, desc.inode, desc.size_real, fs->super.block_size);
+    return st;
+}
+
+void CLocatorAPI::archive::cleanup_sqfs()
+{
+    // cleanup() is inverse autoload(), which does nothing
+}
+
+void CLocatorAPI::archive::close_sqfs()
+{
+    fs->drain();
+
+    sqfs_drop(cmp);
+    cmp = nullptr;
+
+    xr_delete(fs);
+    fs = nullptr;
+
+    sqfs_drop(file);
+    file = nullptr;
+}

.gitmodules
@@ -22,10 +22,19 @@
 [submodule "3rd_party/Src/callme"]
 	path = 3rd_party/Src/callme
 	url = https://github.com/solbjorn/callme.git
+[submodule "3rd_party/Src/concurrentqueue"]
+	path = 3rd_party/Src/concurrentqueue
+	url = https://github.com/cameron314/concurrentqueue.git
+[submodule "3rd_party/Src/libsquashfs/squashfs-tools-ng"]
+	path = 3rd_party/Src/libsquashfs/squashfs-tools-ng
+	url = https://github.com/AgentD/squashfs-tools-ng.git
 [submodule "3rd_party/Src/luajit2/luajit2"]
 	path = 3rd_party/Src/luajit2/luajit2
 	url = https://github.com/openresty/luajit2.git
 	ignore = untracked
+[submodule "3rd_party/Src/lz4/lz4"]
+	path = 3rd_party/Src/lz4/lz4
+	url = https://github.com/lz4/lz4.git
 [submodule "3rd_party/Src/mimalloc/mimalloc"]
 	path = 3rd_party/Src/mimalloc/mimalloc
 	url = https://github.com/microsoft/mimalloc.git
@@ -45,3 +54,6 @@
 [submodule "3rd_party/Src/xxHash/xxHash"]
 	path = 3rd_party/Src/xxHash/xxHash
 	url = https://github.com/Cyan4973/xxHash.git
+[submodule "3rd_party/Src/zstd/zstd"]
+	path = 3rd_party/Src/zstd/zstd
+	url = https://github.com/facebook/zstd.git

3rd_party/Src/libsquashfs/.gitignore
@@ -1,0 +1,1 @@
+_Build/

3rd_party/Src/libsquashfs/CMakeLists.txt
@@ -1,0 +1,134 @@
+cmake_minimum_required(VERSION 3.18)
+project(libsquashfs VERSION 5.0.4 LANGUAGES C)
+
+set(CMAKE_C_STANDARD 23)
+
+set(sqfs_headers
+    squashfs-tools-ng/include/compat.h
+    squashfs-tools-ng/include/sqfs/block.h
+    squashfs-tools-ng/include/sqfs/block_processor.h
+    squashfs-tools-ng/include/sqfs/block_writer.h
+    squashfs-tools-ng/include/sqfs/compressor.h
+    squashfs-tools-ng/include/sqfs/data_reader.h
+    squashfs-tools-ng/include/sqfs/dir.h
+    squashfs-tools-ng/include/sqfs/dir_entry.h
+    squashfs-tools-ng/include/sqfs/dir_reader.h
+    squashfs-tools-ng/include/sqfs/dir_writer.h
+    squashfs-tools-ng/include/sqfs/error.h
+    squashfs-tools-ng/include/sqfs/frag_table.h
+    squashfs-tools-ng/include/sqfs/id_table.h
+    squashfs-tools-ng/include/sqfs/inode.h
+    squashfs-tools-ng/include/sqfs/io.h
+    squashfs-tools-ng/include/sqfs/meta_reader.h
+    squashfs-tools-ng/include/sqfs/meta_writer.h
+    squashfs-tools-ng/include/sqfs/predef.h
+    squashfs-tools-ng/include/sqfs/super.h
+    squashfs-tools-ng/include/sqfs/table.h
+    squashfs-tools-ng/include/sqfs/xattr.h
+    squashfs-tools-ng/include/sqfs/xattr_reader.h
+    squashfs-tools-ng/include/sqfs/xattr_writer.h
+    squashfs-tools-ng/include/util/array.h
+    squashfs-tools-ng/include/util/hash_table.h
+    squashfs-tools-ng/include/util/mempool.h
+    squashfs-tools-ng/include/util/rbtree.h
+    squashfs-tools-ng/include/util/threadpool.h
+)
+
+set(sqfs_sources
+    ${sqfs_headers}
+    squashfs-tools-ng/lib/compat/src/strchrnul.c
+    squashfs-tools-ng/lib/compat/src/strndup.c
+    squashfs-tools-ng/lib/sqfs/src/block_processor/backend.c
+    squashfs-tools-ng/lib/sqfs/src/block_processor/block_processor.c
+    squashfs-tools-ng/lib/sqfs/src/block_processor/frontend.c
+    squashfs-tools-ng/lib/sqfs/src/block_processor/internal.h
+    squashfs-tools-ng/lib/sqfs/src/block_processor/ostream.c
+    squashfs-tools-ng/lib/sqfs/src/block_writer.c
+    squashfs-tools-ng/lib/sqfs/src/comp/compressor.c
+    squashfs-tools-ng/lib/sqfs/src/comp/internal.h
+    squashfs-tools-ng/lib/sqfs/src/comp/lz4.c
+    squashfs-tools-ng/lib/sqfs/src/comp/zstd.c
+    squashfs-tools-ng/lib/sqfs/src/data_reader.c
+    squashfs-tools-ng/lib/sqfs/src/dir_entry.c
+    squashfs-tools-ng/lib/sqfs/src/dir_iterator.c
+    squashfs-tools-ng/lib/sqfs/src/dir_reader.c
+    squashfs-tools-ng/lib/sqfs/src/dir_writer.c
+    squashfs-tools-ng/lib/sqfs/src/frag_table.c
+    squashfs-tools-ng/lib/sqfs/src/id_table.c
+    squashfs-tools-ng/lib/sqfs/src/inode.c
+    squashfs-tools-ng/lib/sqfs/src/io/dir_hl.c
+    squashfs-tools-ng/lib/sqfs/src/io/dir_rec.c
+    squashfs-tools-ng/lib/sqfs/src/io/dir_win32.c
+    squashfs-tools-ng/lib/sqfs/src/io/file.c
+    squashfs-tools-ng/lib/sqfs/src/io/istream.c
+    squashfs-tools-ng/lib/sqfs/src/io/ostream.c
+    squashfs-tools-ng/lib/sqfs/src/io/stream_api.c
+    squashfs-tools-ng/lib/sqfs/src/io/win32.c
+    squashfs-tools-ng/lib/sqfs/src/meta_reader.c
+    squashfs-tools-ng/lib/sqfs/src/meta_writer.c
+    squashfs-tools-ng/lib/sqfs/src/misc.c
+    squashfs-tools-ng/lib/sqfs/src/read_inode.c
+    squashfs-tools-ng/lib/sqfs/src/read_super.c
+    squashfs-tools-ng/lib/sqfs/src/read_table.c
+    squashfs-tools-ng/lib/sqfs/src/readdir.c
+    squashfs-tools-ng/lib/sqfs/src/super.c
+    squashfs-tools-ng/lib/sqfs/src/write_inode.c
+    squashfs-tools-ng/lib/sqfs/src/write_super.c
+    squashfs-tools-ng/lib/sqfs/src/write_table.c
+    squashfs-tools-ng/lib/sqfs/src/xattr/xattr.c
+    squashfs-tools-ng/lib/sqfs/src/xattr/xattr_reader.c
+    squashfs-tools-ng/lib/sqfs/src/xattr/xattr_writer.c
+    squashfs-tools-ng/lib/sqfs/src/xattr/xattr_writer.h
+    squashfs-tools-ng/lib/sqfs/src/xattr/xattr_writer_flush.c
+    squashfs-tools-ng/lib/sqfs/src/xattr/xattr_writer_record.c
+    squashfs-tools-ng/lib/util/src/alloc.c
+    squashfs-tools-ng/lib/util/src/array.c
+    squashfs-tools-ng/lib/util/src/file_cmp.c
+    squashfs-tools-ng/lib/util/src/hash_table.c
+    squashfs-tools-ng/lib/util/src/is_memory_zero.c
+    squashfs-tools-ng/lib/util/src/mempool.c
+    squashfs-tools-ng/lib/util/src/rbtree.c
+    squashfs-tools-ng/lib/util/src/str_table.c
+    squashfs-tools-ng/lib/util/src/threadpool.c
+    squashfs-tools-ng/lib/util/src/xxhash.c
+)
+
+if (NOT DEFINED BUILD_SHARED_LIBS)
+    set(BUILD_SHARED_LIBS ON)
+endif()
+
+if (BUILD_SHARED_LIBS)
+    add_library(libsquashfs SHARED ${sqfs_sources})
+else()
+    add_library(libsquashfs STATIC ${sqfs_sources})
+endif()
+
+if (NOT LZ4_INC)
+    message(SEND_ERROR "LZ4_INC must be set")
+endif()
+
+if (NOT ZSTD_INC)
+    message(SEND_ERROR "ZSTD_INC must be set")
+endif()
+
+target_include_directories(libsquashfs PRIVATE "${CMAKE_SOURCE_DIR}/inc")
+target_include_directories(libsquashfs PRIVATE "${CMAKE_SOURCE_DIR}/squashfs-tools-ng/include")
+target_include_directories(libsquashfs PRIVATE "${LZ4_INC}")
+target_include_directories(libsquashfs PRIVATE "${ZSTD_INC}")
+
+find_library(LZ4_LIB lz4)
+target_link_libraries(libsquashfs ${LZ4_LIB})
+find_library(ZSTD_LIB zstd)
+target_link_libraries(libsquashfs ${ZSTD_LIB})
+
+set_target_properties(libsquashfs PROPERTIES PREFIX "" POSITION_INDEPENDENT_CODE ON)
+target_compile_definitions(libsquashfs PRIVATE WITH_LZ4 WITH_ZSTD)
+
+if (NOT BUILD_SHARED_LIBS)
+    target_compile_definitions(libsquashfs PRIVATE SQFS_STATIC)
+endif()
+
+target_compile_options(libsquashfs PRIVATE "$<$<CONFIG:Release>:/Zi>")
+target_link_options(libsquashfs PRIVATE "$<$<CONFIG:Release>:/DEBUG>")
+target_link_options(libsquashfs PRIVATE "$<$<CONFIG:Release>:/OPT:REF>")
+target_link_options(libsquashfs PRIVATE "$<$<CONFIG:Release>:/OPT:ICF>")

commit 874537cf8d407ae214dfe49aeab858d521891f78

xrCore: optimize and make SquashFS readers fully thread-safe
libsquashfs from squashfs-tools-ng doesn't care about
multi-threading and doesn't provide any guarantees. That's why I
initially made a "reader" class which always has its own file ref,
directory, and file readers.
What I missed is that sqfs_compressor_t should not be shared as well.
Everything worked for me, but not for everyone.

Make readers have their own compressor to avoid the refcount mess.
Move the compressor config struct to the pool to initialize
compressors faster. compressor_config_t, as its name says, contains
only configuration options, no pointers or refcounts, so it can be
shared the same way as we share sqfs_super_t.
This also simplifies the entire logic -- no more global sqfs_file_t,
every operation must be done using a reader. The initial file used
to read the superblock gets destroyed right after the operation.

Fixes: e5d9683ac7f3 ("xrCore: add support for SquashFS archives (lol)")
Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrCore/LocatorAPI.h
@@ -12,7 +12,6 @@ class CStreamReader;
 
 namespace sqfs
 {
-struct sqfs_compressor_t;
 struct sqfs_dir_iterator_t;
 struct sqfs_file_t;
 } // namespace sqfs
@@ -76,19 +75,14 @@ private:
         union
         {
             // SquashFS
-            struct
-            {
-                sqfs::sqfs_file_t* file{};
-                sqfs::sqfs_compressor_t* cmp{};
-                xr_sqfs* fs{};
-            };
+            xr_sqfs* fs;
 
             // DB
             struct
             {
-                void* hSrcMap;
-                CInifile* header;
-                u32 key;
+                void* hSrcMap{};
+                CInifile* header{};
+                u32 key{};
             };
         };
 

ogsr_engine/xrCore/arc_sqfs.cpp
@@ -25,6 +25,12 @@ XR_DIAG_POP();
 } // namespace sqfs
 
 template <>
+struct std::default_delete<sqfs::sqfs_compressor_t>
+{
+    constexpr void operator()(sqfs::sqfs_compressor_t* ptr) const noexcept { sqfs::sqfs_drop(ptr); }
+};
+
+template <>
 struct std::default_delete<sqfs::sqfs_data_reader_t>
 {
     constexpr void operator()(sqfs::sqfs_data_reader_t* ptr) const noexcept { sqfs::sqfs_drop(ptr); }
@@ -73,6 +79,7 @@ public:
     {
     public:
         std::unique_ptr<sqfs::sqfs_file_t> file;
+        std::unique_ptr<sqfs::sqfs_compressor_t> cmp;
         std::unique_ptr<sqfs::sqfs_dir_reader_t> dr;
         std::unique_ptr<sqfs::sqfs_data_reader_t> data;
 
@@ -91,13 +98,24 @@ private:
     pool_t pool;
 
 public:
-    sqfs::sqfs_super_t super{};
+    sqfs::sqfs_super_t super;
+    sqfs::sqfs_compressor_config_t cfg;
 
     xr_sqfs() = delete;
-    explicit xr_sqfs(sqfs::sqfs_file_t& file) { R_ASSERT(sqfs::sqfs_super_read(&super, &file) == 0); }
+    explicit xr_sqfs(gsl::czstring path);
 
     [[nodiscard]] auto acquire_scoped(const archive& arc) { return pool.acquire_scoped(arc); }
 };
+
+CLocatorAPI::archive::xr_sqfs::xr_sqfs(gsl::czstring path)
+{
+    sqfs::sqfs_file_t* f;
+    R_ASSERT(sqfs::sqfs_file_open(&f, path, sqfs::SQFS_FILE_OPEN_READ_ONLY | sqfs::SQFS_FILE_OPEN_NO_CHARSET_XFRM) == 0);
+    const auto file = absl::WrapUnique(f);
+
+    R_ASSERT(sqfs::sqfs_super_read(&super, file.get()) == 0);
+    R_ASSERT(sqfs::sqfs_compressor_config_init(&cfg, gsl::narrow<sqfs::SQFS_COMPRESSOR>(super.compression_id), super.block_size, sqfs::SQFS_COMP_FLAG_UNCOMPRESS) == 0);
+}
 
 CLocatorAPI::archive::xr_sqfs::reader::reader(const archive& arc)
 {
@@ -105,8 +123,12 @@ CLocatorAPI::archive::xr_sqfs::reader::reader(const archive& arc)
     R_ASSERT(sqfs::sqfs_file_open(&f, arc.path.c_str(), sqfs::SQFS_FILE_OPEN_READ_ONLY | sqfs::SQFS_FILE_OPEN_NO_CHARSET_XFRM) == 0);
     file = absl::WrapUnique(f);
 
-    dr = absl::WrapUnique(sqfs::sqfs_dir_reader_create(&arc.fs->super, arc.cmp, file.get(), 0));
-    data = absl::WrapUnique(sqfs::sqfs_data_reader_create(file.get(), arc.fs->super.block_size, arc.cmp, 0));
+    sqfs::sqfs_compressor_t* c;
+    R_ASSERT(sqfs::sqfs_compressor_create(&arc.fs->cfg, &c) == 0);
+    cmp = absl::WrapUnique(c);
+
+    dr = absl::WrapUnique(sqfs::sqfs_dir_reader_create(&arc.fs->super, cmp.get(), file.get(), 0));
+    data = absl::WrapUnique(sqfs::sqfs_data_reader_create(file.get(), arc.fs->super.block_size, cmp.get(), 0));
     R_ASSERT(dr && data);
 
     R_ASSERT(sqfs::sqfs_data_reader_load_fragment_table(data.get(), &arc.fs->super) == 0);
@@ -240,14 +262,7 @@ CStreamReader* CLocatorAPI::archive::xr_sqfs_stream::open_chunk(u32 chunk_id)
 void CLocatorAPI::archive::open_sqfs()
 {
     type = container::SQFS;
-
-    R_ASSERT(sqfs::sqfs_file_open(&file, path.c_str(), sqfs::SQFS_FILE_OPEN_READ_ONLY | sqfs::SQFS_FILE_OPEN_NO_CHARSET_XFRM) == 0);
-    fs = xr_new<xr_sqfs>(*file);
-
-    sqfs::sqfs_compressor_config_t cfg;
-    sqfs::sqfs_compressor_config_init(&cfg, gsl::narrow<sqfs::SQFS_COMPRESSOR>(fs->super.compression_id), fs->super.block_size, sqfs::SQFS_COMP_FLAG_UNCOMPRESS);
-
-    R_ASSERT(sqfs::sqfs_compressor_create(&cfg, &cmp) == 0);
+    fs = xr_new<xr_sqfs>(path.c_str());
 }
 
 bool CLocatorAPI::archive::autoload_sqfs()
@@ -304,11 +319,11 @@ void CLocatorAPI::archive::index_dir_sqfs(CLocatorAPI& loc, gsl::czstring path, 
 
 void CLocatorAPI::archive::index_sqfs(CLocatorAPI& loc, gsl::czstring fs_entry_point) const
 {
-    const auto idt = absl::WrapUnique(sqfs::sqfs_id_table_create(0));
-    R_ASSERT(idt && sqfs::sqfs_id_table_read(idt.get(), file, &fs->super, cmp) == 0);
-
     const auto obj = fs->acquire_scoped(*this);
     const auto& rd = obj.value;
+
+    const auto idt = absl::WrapUnique(sqfs::sqfs_id_table_create(0));
+    R_ASSERT(idt && sqfs::sqfs_id_table_read(idt.get(), rd.file.get(), &fs->super, rd.cmp.get()) == 0);
 
     const auto it = [&idt, &rd] {
         sqfs::sqfs_inode_generic_t* inp;
@@ -349,13 +364,4 @@ void CLocatorAPI::archive::cleanup_sqfs()
     // cleanup() is inverse autoload(), which does nothing
 }
 
-void CLocatorAPI::archive::close_sqfs()
-{
-    sqfs::sqfs_drop(cmp);
-    cmp = nullptr;
-
-    xr_delete(fs);
-
-    sqfs::sqfs_drop(file);
-    file = nullptr;
-}
+void CLocatorAPI::archive::close_sqfs() { xr_delete(fs); }