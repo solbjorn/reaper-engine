solbjorn/reaper-engine

commit be1bbde569a24a8bc636773b0a5e7556c2735766

xrCore: strictly-typed X-Ray pt I
* xrCore is now free of -Wconversion -Wnarrowing -Wold-style-cast;
* most of constructors in xrCore and lots in other places are now
  explicit. This allowed to optimize different pieces of code
  throughout the engine, notably the removal of an implicit
  czstring <-> shared_str conversion showed tons of places where
  the shared string container was used without reason on was emitting
  additional copying etc.
* all timestamps and file sizes are now of s64, no more 32 bits. Except
  for XDB which has u32s in its headers, so no chance for 64-bit file
  support;
* partial fix for trivial types like vectors, but with custom copy/move
  constructors to use 64-bit or 128-bit writes. [[clang::trivial_abi]]
  helped, but currently doesn't work well with the Windows/MSVC ABI, so
  more can be done in future;
* IReader now has its data pointer const, which uncovered lots of places
  to optimize (several class methods were reading directly from IReaders,
  so making them return const pointers helped);
* XR_SYSV is dropped -- since it can't be used for constructors, the
  whole idea of making the whole engine use the SysV ABI is not feasible
  currently; and marking only some functions brings more harm than good
  (calling a SysV function from an MS function costs a dozen of
  instructions due to the XMM clobbering).
  If/when Clang is able to apply the attribute to constructors and
  destructors and ideally using a pragma instead of a per-function
  attribute, I might go back try this again;
* lots of optimization, constexpr, hardening, etc.

Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrCore/FS.cpp
@@ -72,7 +72,7 @@ void VerifyPath(absl::string_view path)
 // memory
 CMemoryWriter::~CMemoryWriter() { xr_free(data); }
 
-void CMemoryWriter::w(const void* ptr, size_t count)
+void CMemoryWriter::w(const void* ptr, gsl::index count)
 {
     if (position + count > mem_size)
     {
@@ -83,21 +83,22 @@ void CMemoryWriter::w(const void* ptr, size_t count)
             mem_size *= 2;
 
         if (!data)
-            data = (BYTE*)xr_malloc(mem_size);
+            data = static_cast<std::byte*>(xr_malloc(mem_size));
         else
-            data = (BYTE*)xr_realloc(data, mem_size);
+            data = static_cast<std::byte*>(xr_realloc(data, mem_size));
     }
 
-    CopyMemory(data + position, ptr, count);
+    std::memcpy(data + position, ptr, gsl::narrow_cast<size_t>(count));
     position += count;
+
     if (position > file_size)
         file_size = position;
 }
 
-void CMemoryWriter::reserve(const size_t count)
+void CMemoryWriter::reserve(gsl::index count)
 {
+    data = static_cast<std::byte*>(xr_malloc(mem_size));
     mem_size = count;
-    data = (BYTE*)xr_malloc(mem_size);
 }
 
 bool CMemoryWriter::save_to(LPCSTR fn) const
@@ -118,27 +119,30 @@ void IWriter::open_chunk(u32 type)
     chunk_pos.push(tell());
     w_u32(0); // the place for 'size'
 }
+
 void IWriter::close_chunk()
 {
     VERIFY(!chunk_pos.empty());
 
-    const size_t pos = tell();
+    const auto pos = tell();
     seek(chunk_pos.top());
-    w_u32(pos - chunk_pos.top() - 4);
+    w_u32(gsl::narrow<u32>(pos - chunk_pos.top() - 4));
     seek(pos);
     chunk_pos.pop();
 }
-size_t IWriter::chunk_size()
+
+gsl::index IWriter::chunk_size()
 {
     if (chunk_pos.empty())
         return 0;
+
     return tell() - chunk_pos.top() - 4;
 }
 
-void IWriter::w_compressed(void* ptr, size_t count, const bool encrypt, const bool is_ww)
+void IWriter::w_compressed(void* ptr, gsl::index count, bool encrypt, bool is_ww)
 {
     BYTE* dest{};
-    size_t dest_sz{};
+    gsl::index dest_sz{};
     _compressLZ(&dest, &dest_sz, ptr, count);
 
     if (encrypt)
@@ -149,7 +153,7 @@ void IWriter::w_compressed(void* ptr, size_t count, const bool encrypt, const bo
     xr_free(dest);
 }
 
-void IWriter::w_chunk(u32 type, void* data, size_t size, const bool encrypt, const bool is_ww)
+void IWriter::w_chunk(u32 type, void* data, gsl::index size, bool encrypt, bool is_ww)
 {
     open_chunk(type);
 
@@ -160,6 +164,7 @@ void IWriter::w_chunk(u32 type, void* data, size_t size, const bool encrypt, con
 
     close_chunk();
 }
+
 void IWriter::w_sdir(const Fvector& D)
 {
     Fvector C;
@@ -176,6 +181,7 @@ void IWriter::w_sdir(const Fvector& D)
     w_dir(C);
     w_float(mag);
 }
+
 void IWriter::w_printf(const char* format, ...)
 {
     va_list mark;
@@ -190,14 +196,15 @@ void IWriter::w_printf(const char* format, ...)
 IReader* IReader::open_chunk(u32 ID)
 {
     BOOL bCompressed;
-    const size_t dwSize = find_chunk(ID, &bCompressed);
+    const auto dwSize = find_chunk(ID, &bCompressed);
     if (dwSize != 0)
     {
         if (bCompressed)
         {
-            BYTE* dest;
-            size_t dest_sz;
-            _decompressLZ(&dest, &dest_sz, pointer(), dwSize);
+            BYTE* dest{};
+            gsl::index dest_sz{};
+
+            std::ignore = _decompressLZ(&dest, &dest_sz, pointer(), dwSize);
             return xr_new<CTempReader>(dest, dest_sz, tell() + dwSize);
         }
         else
@@ -230,25 +237,26 @@ IReader* IReader::open_chunk_iterator(u32& ID, IReader* _prev)
     }
 
     //	open
-    if (elapsed() < static_cast<long>(sizeof(u32) * 2))
+    if (elapsed() < gsl::index{sizeof(u32) * 2})
         return nullptr;
 
     ID = r_u32();
-    size_t _size = r_u32();
+    gsl::index _size{r_u32()};
 
     // На всякий случай тут тоже так сделаем по аналогии с find_chunk()
-    if (elapsed() < static_cast<long>(_size))
+    if (elapsed() < _size)
     {
-        Msg("!![%s] chunk [%u] has invalid size [%zu], return elapsed size [%zd]", __FUNCTION__, ID, _size, elapsed());
+        Msg("!![%s] chunk [%u] has invalid size [%zd], return elapsed size [%zd]", __FUNCTION__, ID, _size, elapsed());
         _size = elapsed();
     }
 
     if (ID & CFS_CompressMark)
     {
         // compressed
-        u8* dest;
-        size_t dest_sz;
-        _decompressLZ(&dest, &dest_sz, pointer(), _size);
+        u8* dest{};
+        gsl::index dest_sz{};
+
+        std::ignore = _decompressLZ(&dest, &dest_sz, pointer(), _size);
         return xr_new<CTempReader>(dest, dest_sz, tell() + _size);
     }
     else
@@ -279,11 +287,12 @@ void IReader::skip_bom(const char* dbg_name)
     Msg("! Skip BOM for file [%s]", dbg_name);
 }
 
-void IReader::r(void* p, size_t cnt)
+void IReader::r(void* p, gsl::index cnt)
 {
     R_ASSERT(Pos + cnt <= Size);
-    CopyMemory(p, pointer(), cnt);
+    std::memcpy(p, pointer(), gsl::narrow_cast<size_t>(cnt));
     advance(cnt);
+
 #ifdef DEBUG
     BOOL bShow = FALSE;
     if (smart_cast<CVirtualFileReader*>(this))
@@ -295,12 +304,16 @@ void IReader::r(void* p, size_t cnt)
 #endif
 }
 
-constexpr bool is_term(const char a) { return a == '\r' || a == '\n'; }
-
-IC u32 IReader::advance_term_string()
+namespace
 {
-    size_t sz = 0;
-    char* src = (char*)data;
+constexpr bool is_term(char a) { return a == '\r' || a == '\n'; }
+} // namespace
+
+gsl::index IReader::advance_term_string()
+{
+    const char* src = reinterpret_cast<const char*>(data);
+    gsl::index sz{};
+
     while (!eof())
     {
         Pos++;
@@ -313,29 +326,31 @@ IC u32 IReader::advance_term_string()
             break;
         }
     }
+
     return sz;
 }
 
-void IReader::r_string(char* dest, size_t tgt_sz)
+void IReader::r_string(char* dest, gsl::index tgt_sz)
 {
-    char* src = (char*)data + Pos;
-    size_t sz = advance_term_string();
+    const char* src = reinterpret_cast<const char*>(data) + Pos;
+    const auto sz = advance_term_string();
     R_ASSERT2(sz < (tgt_sz - 1), "Dest string less than needed.");
-    strncpy(dest, src, sz);
-    dest[sz] = 0;
+
+    strncpy(dest, src, gsl::narrow_cast<size_t>(sz));
+    dest[sz] = '\0';
 }
 
 void IReader::r_string(xr_string& dest)
 {
-    char* src = (char*)data + Pos;
-    size_t sz = advance_term_string();
-    dest.assign(src, sz);
+    const char* src = reinterpret_cast<const char*>(data) + Pos;
+    const auto sz = advance_term_string();
+    dest.assign(src, gsl::narrow_cast<size_t>(sz));
 }
 
-void IReader::r_stringZ(char* dest, size_t tgt_sz)
+void IReader::r_stringZ(char* dest, gsl::index tgt_sz)
 {
-    char* src = (char*)data;
-    size_t sz = 0;
+    const char* src = reinterpret_cast<const char*>(data);
+    gsl::index sz{};
 
     while ((src[Pos] != 0) && (!eof()))
     {
@@ -352,9 +367,9 @@ void IReader::r_stringZ(char* dest, size_t tgt_sz)
 
 void IReader::r_stringZ(shared_str& dest)
 {
-    char* src = (char*)(data + Pos);
+    const char* src = reinterpret_cast<const char*>(data) + Pos;
+    gsl::index size{};
 
-    size_t size = 0;
     while ((src[size] != 0) && (!eof()))
     {
         size++;
@@ -362,29 +377,28 @@ void IReader::r_stringZ(shared_str& dest)
     }
 
     std::string tmp;
-    tmp.assign(src, size);
+    tmp.assign(src, gsl::narrow_cast<size_t>(size));
 
-    dest = tmp.c_str();
+    dest._set(tmp.c_str());
 
     // advance(size);
 
     if (!eof())
         Pos++;
 }
+
 void IReader::r_stringZ(xr_string& dest)
 {
-    char* src = (char*)(data + Pos);
+    const char* src = reinterpret_cast<const char*>(data) + Pos;
+    gsl::index size{};
 
-    size_t size = 0;
     while ((src[size] != 0) && (!eof()))
     {
         size++;
         Pos++;
     }
 
-    dest.assign(src, size);
-
-    // advance(size);
+    dest.assign(src, gsl::narrow_cast<size_t>(size));
 
     if (!eof())
         Pos++;
@@ -392,7 +406,8 @@ void IReader::r_stringZ(xr_string& dest)
 
 void IReader::skip_stringZ()
 {
-    char* src = (char*)data;
+    const char* src = reinterpret_cast<const char*>(data);
+
     while ((src[Pos] != 0) && (!eof()))
         Pos++;
 
@@ -401,7 +416,13 @@ void IReader::skip_stringZ()
 
 //---------------------------------------------------
 // temp stream
-CTempReader::~CTempReader() { xr_free(data); }
+CTempReader::~CTempReader()
+{
+    auto ptr = const_cast<std::byte*>(data);
+    xr_free(ptr);
+
+    data = nullptr;
+}
 
 //---------------------------------------------------
 // pack stream
@@ -415,7 +436,7 @@ CPackReader::~CPackReader()
 }
 //---------------------------------------------------
 
-CVirtualFileReader::CVirtualFileReader(const char* cFileName)
+CVirtualFileReader::CVirtualFileReader(gsl::czstring cFileName)
 {
     Pos = 0;
 
@@ -433,7 +454,7 @@ CVirtualFileReader::CVirtualFileReader(const char* cFileName)
     hSrcMap = CreateFileMapping(hSrcFile, nullptr, PAGE_READONLY, 0, 0, nullptr);
     R_ASSERT3(hSrcMap != INVALID_HANDLE_VALUE, cFileName, Debug.error2string(GetLastError()));
 
-    data = (char*)MapViewOfFile(hSrcMap, FILE_MAP_READ, 0, 0, 0);
+    data = static_cast<const std::byte*>(MapViewOfFile(hSrcMap, FILE_MAP_READ, 0, 0, 0));
     R_ASSERT3(!Size || data, cFileName, Debug.error2string(GetLastError()));
 
 #ifdef DEBUG
@@ -447,7 +468,7 @@ CVirtualFileReader::~CVirtualFileReader()
     unregister_file_mapping(data, Size);
 #endif // DEBUG
 
-    UnmapViewOfFile((void*)data);
+    UnmapViewOfFile(data);
     CloseHandle(hSrcMap);
     CloseHandle(hSrcFile);
 }

ogsr_engine/xrCore/_color.h
@@ -1,100 +1,108 @@
 #pragma once
 
 // maps unsigned 8 bits/channel to D3DCOLOR
-constexpr u32 color_argb(u32 a, u32 r, u32 g, u32 b) noexcept { return ((a & 0xff) << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff); }
-constexpr u32 color_rgba(u32 r, u32 g, u32 b, u32 a) noexcept { return color_argb(a, r, g, b); }
+[[nodiscard]] constexpr u32 color_argb(u32 a, u32 r, u32 g, u32 b) noexcept { return ((a & 0xff) << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff); }
+[[nodiscard]] constexpr u32 color_rgba(u32 r, u32 g, u32 b, u32 a) noexcept { return color_argb(a, r, g, b); }
 
-ICF u32 color_argb_f(f32 a, f32 r, f32 g, f32 b) noexcept
+[[nodiscard]] constexpr u32 color_argb_f(f32 a, f32 r, f32 g, f32 b) noexcept
 {
-    s32 _r = clampr(iFloor(r * 255.f), 0, 255);
-    s32 _g = clampr(iFloor(g * 255.f), 0, 255);
-    s32 _b = clampr(iFloor(b * 255.f), 0, 255);
-    s32 _a = clampr(iFloor(a * 255.f), 0, 255);
+    const auto _r{std::clamp(gsl::narrow_cast<u32>(std::floor(r * 255.0f)), 0u, 255u)};
+    const auto _g{std::clamp(gsl::narrow_cast<u32>(std::floor(g * 255.0f)), 0u, 255u)};
+    const auto _b{std::clamp(gsl::narrow_cast<u32>(std::floor(b * 255.0f)), 0u, 255u)};
+    const auto _a{std::clamp(gsl::narrow_cast<u32>(std::floor(a * 255.0f)), 0u, 255u)};
+
     return color_argb(_a, _r, _g, _b);
 }
-ICF u32 color_rgba_f(f32 r, f32 g, f32 b, f32 a) noexcept { return color_argb_f(a, r, g, b); }
 
-constexpr u32 color_xrgb(u32 r, u32 g, u32 b) noexcept { return color_argb(0xff, r, g, b); }
+[[nodiscard]] constexpr u32 color_rgba_f(f32 r, f32 g, f32 b, f32 a) noexcept { return color_argb_f(a, r, g, b); }
+[[nodiscard]] constexpr u32 color_xrgb(u32 r, u32 g, u32 b) noexcept { return color_argb(0xff, r, g, b); }
 
-constexpr u32 color_get_R(u32 rgba) noexcept { return (((rgba) >> 16) & 0xff); }
-constexpr u32 color_get_G(u32 rgba) noexcept { return (((rgba) >> 8) & 0xff); }
-constexpr u32 color_get_B(u32 rgba) noexcept { return ((rgba) & 0xff); }
-constexpr u32 color_get_A(u32 rgba) noexcept { return ((rgba) >> 24); }
+[[nodiscard]] constexpr u32 color_get_R(u32 rgba) noexcept { return (((rgba) >> 16) & 0xff); }
+[[nodiscard]] constexpr u32 color_get_G(u32 rgba) noexcept { return (((rgba) >> 8) & 0xff); }
+[[nodiscard]] constexpr u32 color_get_B(u32 rgba) noexcept { return ((rgba) & 0xff); }
+[[nodiscard]] constexpr u32 color_get_A(u32 rgba) noexcept { return ((rgba) >> 24); }
 
-constexpr u32 subst_alpha(u32 rgba, u32 a) noexcept { return (rgba & ~color_rgba(0, 0, 0, 0xff)) | color_rgba(0, 0, 0, a); }
+[[nodiscard]] constexpr u32 subst_alpha(u32 rgba, u32 a) noexcept { return (rgba & ~color_rgba(0, 0, 0, 0xff)) | color_rgba(0, 0, 0, a); }
 
-constexpr u32 bgr2rgb(u32 bgr) noexcept { return color_rgba(color_get_B(bgr), color_get_G(bgr), color_get_R(bgr), 0); }
-constexpr u32 rgb2bgr(u32 rgb) noexcept { return bgr2rgb(rgb); }
+[[nodiscard]] constexpr u32 bgr2rgb(u32 bgr) noexcept { return color_rgba(color_get_B(bgr), color_get_G(bgr), color_get_R(bgr), 0); }
+[[nodiscard]] constexpr u32 rgb2bgr(u32 rgb) noexcept { return bgr2rgb(rgb); }
 
-struct Fcolor
+struct XR_TRIVIAL Fcolor
 {
-    float r, g, b, a;
-
-    Fcolor() noexcept = default;
-
-    constexpr Fcolor(float _r, float _g, float _b, float _a) noexcept : r(_r), g(_g), b(_b), a(_a) {}
-
-    Fcolor(u32 dw) noexcept
+    union
     {
-        constexpr float f = 1.f / 255.f;
-        a = f * float((dw >> 24) & 0xff);
-        r = f * float((dw >> 16) & 0xff);
-        g = f * float((dw >> 8) & 0xff);
-        b = f * float((dw >> 0) & 0xff);
-    }
+        struct
+        {
+            f32 r, g, b, a;
+        };
+        std::array<f32, 4> arr;
+    };
 
-    constexpr Fcolor& set(u32 dw) noexcept
-    {
-        constexpr float f = 1.f / 255.f;
-        a = f * float((dw >> 24) & 0xff);
-        r = f * float((dw >> 16) & 0xff);
-        g = f * float((dw >> 8) & 0xff);
-        b = f * float((dw >> 0) & 0xff);
-        return *this;
-    }
+    constexpr Fcolor() noexcept = default;
+    constexpr explicit Fcolor(f32 _r, f32 _g, f32 _b, f32 _a) noexcept : r{_r}, g{_g}, b{_b}, a{_a} {}
+    constexpr explicit Fcolor(u32 dw) noexcept { set(dw); }
+
+    constexpr Fcolor(const Fcolor& that) noexcept { set(that); }
+    constexpr Fcolor(Fcolor&&) noexcept = default;
 
     constexpr Fcolor& operator=(u32 dw) noexcept { return set(dw); }
 
-    constexpr Fcolor& set(float _r, float _g, float _b, float _a) noexcept
+    constexpr Fcolor& operator=(const Fcolor& that) noexcept { return set(that); }
+    constexpr Fcolor& operator=(Fcolor&&) noexcept = default;
+
+    constexpr Fcolor& set(u32 dw) noexcept
+    {
+        constexpr f32 f{1.0f / 255.0f};
+
+        a = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 24));
+        b = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 16));
+        g = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 8));
+        r = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw));
+
+        return *this;
+    }
+
+    constexpr Fcolor& set(f32 _r, f32 _g, f32 _b, f32 _a) noexcept
     {
         r = _r;
         g = _g;
         b = _b;
         a = _a;
+
         return *this;
     }
 
-    constexpr Fcolor& set(const Fcolor& rhs) noexcept
+    constexpr Fcolor& set(const Fcolor& that) noexcept
     {
-        r = rhs.r;
-        g = rhs.g;
-        b = rhs.b;
-        a = rhs.a;
+        arr = that.arr;
         return *this;
     }
 
-    u32 get() const noexcept { return color_rgba_f(r, g, b, a); }
+    [[nodiscard]] constexpr u32 get() const noexcept { return color_rgba_f(r, g, b, a); }
 
-    constexpr u32 get_windows() const noexcept // Get color as a Windows DWORD value.
+    [[nodiscard]] constexpr u32 get_windows() const noexcept // Get color as a Windows DWORD value.
     {
-        const u8 _a = u8(a * 255.f);
-        const u8 _r = u8(r * 255.f);
-        const u8 _g = u8(g * 255.f);
-        const u8 _b = u8(b * 255.f);
-        return (u32)(_a << 24) | (_b << 16) | (_g << 8) | _r;
+        const auto _a{gsl::narrow_cast<u8>(a * 255.0f)};
+        const auto _r{gsl::narrow_cast<u8>(r * 255.0f)};
+        const auto _g{gsl::narrow_cast<u8>(g * 255.0f)};
+        const auto _b{gsl::narrow_cast<u8>(b * 255.0f)};
+
+        return (u32{_a} << 24) | (u32{_b} << 16) | (u32{_g} << 8) | _r;
     }
 
     constexpr Fcolor& set_windows(u32 dw) noexcept // Set color from a Windows DWORD color value.
     {
-        const float f = 1.0f / 255.0f;
-        a = f * (float)(u8)(dw >> 24);
-        b = f * (float)(u8)(dw >> 16);
-        g = f * (float)(u8)(dw >> 8);
-        r = f * (float)(u8)(dw >> 0);
+        constexpr f32 f{1.0f / 255.0f};
+
+        a = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 24));
+        b = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 16));
+        g = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 8));
+        r = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw));
+
         return *this;
     }
 
-    constexpr Fcolor& adjust_contrast(float f) noexcept // >1 - contrast will be increased
+    constexpr Fcolor& adjust_contrast(f32 f) noexcept // >1 - contrast will be increased
     {
         r = 0.5f + f * (r - 0.5f);
         g = 0.5f + f * (g - 0.5f);
@@ -102,7 +110,7 @@ struct Fcolor
         return *this;
     }
 
-    constexpr Fcolor& adjust_contrast(const Fcolor& in, float f) noexcept // >1 - contrast will be increased
+    constexpr Fcolor& adjust_contrast(const Fcolor& in, f32 f) noexcept // >1 - contrast will be increased
     {
         r = 0.5f + f * (in.r - 0.5f);
         g = 0.5f + f * (in.g - 0.5f);
@@ -110,25 +118,29 @@ struct Fcolor
         return *this;
     }
 
-    constexpr Fcolor& adjust_saturation(float s) noexcept
+    constexpr Fcolor& adjust_saturation(f32 s) noexcept
     {
         // Approximate values for each component's contribution to luminance.
         // Based upon the NTSC standard described in ITU-R Recommendation BT.709.
-        const float grey = r * 0.2125f + g * 0.7154f + b * 0.0721f;
+        const f32 grey{r * 0.2125f + g * 0.7154f + b * 0.0721f};
+
         r = grey + s * (r - grey);
         g = grey + s * (g - grey);
         b = grey + s * (b - grey);
+
         return *this;
     }
 
-    constexpr Fcolor& adjust_saturation(const Fcolor& in, float s) noexcept
+    constexpr Fcolor& adjust_saturation(const Fcolor& in, f32 s) noexcept
     {
         // Approximate values for each component's contribution to luminance.
         // Based upon the NTSC standard described in ITU-R Recommendation BT.709.
-        const float grey = in.r * 0.2125f + in.g * 0.7154f + in.b * 0.0721f;
+        const f32 grey{in.r * 0.2125f + in.g * 0.7154f + in.b * 0.0721f};
+
         r = grey + s * (in.r - grey);
         g = grey + s * (in.g - grey);
         b = grey + s * (in.b - grey);
+
         return *this;
     }
 
@@ -138,6 +150,7 @@ struct Fcolor
         g *= in.g;
         b *= in.b;
         a *= in.a;
+
         return *this;
     }
 
@@ -147,6 +160,7 @@ struct Fcolor
         g = in1.g * in2.g;
         b = in1.b * in2.b;
         a = in1.a * in2.a;
+
         return *this;
     }
 
@@ -156,6 +170,7 @@ struct Fcolor
         g = 1.0f - in.g;
         b = 1.0f - in.b;
         a = 1.0f - in.a;
+
         return *this;
     }
 
@@ -165,117 +180,129 @@ struct Fcolor
         g = 1.0f - g;
         b = 1.0f - b;
         a = 1.0f - a;
+
         return *this;
     }
 
-    constexpr Fcolor& sub_rgb(float s) noexcept
+    constexpr Fcolor& sub_rgb(f32 s) noexcept
     {
         r -= s;
         g -= s;
         b -= s;
+
         return *this;
     }
 
-    constexpr Fcolor& add_rgb(float s) noexcept
+    constexpr Fcolor& add_rgb(f32 s) noexcept
     {
         r += s;
         g += s;
         b += s;
+
         return *this;
     }
 
-    constexpr Fcolor& add_rgba(float s) noexcept
+    constexpr Fcolor& add_rgba(f32 s) noexcept
     {
         r += s;
         g += s;
         b += s;
         a += s;
+
         return *this;
     }
 
-    constexpr Fcolor& mul_rgba(float s) noexcept
+    constexpr Fcolor& mul_rgba(f32 s) noexcept
     {
         r *= s;
         g *= s;
         b *= s;
         a *= s;
+
         return *this;
     }
 
-    constexpr Fcolor& mul_rgb(float s) noexcept
+    constexpr Fcolor& mul_rgb(f32 s) noexcept
     {
         r *= s;
         g *= s;
         b *= s;
+
         return *this;
     }
 
-    constexpr Fcolor& mul_rgba(const Fcolor& c, float s) noexcept
+    constexpr Fcolor& mul_rgba(const Fcolor& c, f32 s) noexcept
     {
         r = c.r * s;
         g = c.g * s;
         b = c.b * s;
         a = c.a * s;
+
         return *this;
     }
 
-    constexpr Fcolor& mul_rgb(const Fcolor& c, float s) noexcept
+    constexpr Fcolor& mul_rgb(const Fcolor& c, f32 s) noexcept
     {
         r = c.r * s;
         g = c.g * s;
         b = c.b * s;
+
         return *this;
     }
 
     // SQ magnitude
-    constexpr float magnitude_sqr_rgb() const noexcept { return r * r + g * g + b * b; }
-
+    [[nodiscard]] constexpr f32 magnitude_sqr_rgb() const noexcept { return r * r + g * g + b * b; }
     // magnitude
-    float magnitude_rgb() const noexcept { return _sqrt(magnitude_sqr_rgb()); }
+    [[nodiscard]] constexpr f32 magnitude_rgb() const noexcept { return _sqrt(magnitude_sqr_rgb()); }
 
-    constexpr float intensity() const noexcept
+    [[nodiscard]] constexpr f32 intensity() const noexcept
     {
         // XXX: Use the component percentages from adjust_saturation()?
         return (r + g + b) / 3.f;
     }
 
     // Normalize
-    Fcolor& normalize_rgb() noexcept
+    constexpr Fcolor& normalize_rgb() noexcept
     {
         VERIFY(magnitude_sqr_rgb() > EPS_S);
+
         return mul_rgb(1.f / magnitude_rgb());
     }
 
-    Fcolor& normalize_rgb(const Fcolor& c) noexcept
+    constexpr Fcolor& normalize_rgb(const Fcolor& c) noexcept
     {
         VERIFY(c.magnitude_sqr_rgb() > EPS_S);
+
         return mul_rgb(c, 1.f / c.magnitude_rgb());
     }
 
-    constexpr Fcolor& lerp(const Fcolor& c1, const Fcolor& c2, float t) noexcept
+    constexpr Fcolor& lerp(const Fcolor& c1, const Fcolor& c2, f32 t) noexcept
     {
-        const float invt = 1.f - t;
+        const f32 invt{1.f - t};
+
         r = c1.r * invt + c2.r * t;
         g = c1.g * invt + c2.g * t;
         b = c1.b * invt + c2.b * t;
         a = c1.a * invt + c2.a * t;
+
         return *this;
     }
 
-    constexpr Fcolor& lerp(const Fcolor& c1, const Fcolor& c2, const Fcolor& c3, float t) noexcept
+    constexpr Fcolor& lerp(const Fcolor& c1, const Fcolor& c2, const Fcolor& c3, f32 t) noexcept
     {
-        if (t > .5f)
+        if (t > 0.5f)
             return lerp(c2, c3, t * 2.f - 1.f);
         else
             return lerp(c1, c2, t * 2.f);
     }
 
-    constexpr inline bool similar_rgba(const Fcolor& v, float E = EPS_L) const noexcept
+    [[nodiscard]] constexpr bool similar_rgba(const Fcolor& v, f32 E = EPS_L) const noexcept
     {
         return fsimilar(r, v.r, E) && fsimilar(g, v.g, E) && fsimilar(b, v.b, E) && fsimilar(a, v.a, E);
     }
 
-    constexpr inline bool similar_rgb(const Fcolor& v, float E = EPS_L) const noexcept { return fsimilar(r, v.r, E) && fsimilar(g, v.g, E) && fsimilar(b, v.b, E); }
+    [[nodiscard]] constexpr bool similar_rgb(const Fcolor& v, f32 E = EPS_L) const noexcept { return fsimilar(r, v.r, E) && fsimilar(g, v.g, E) && fsimilar(b, v.b, E); }
 };
+XR_TRIVIAL_ASSERT(Fcolor);
 
-IC bool _valid(const Fcolor& c) noexcept { return _valid(c.r) && _valid(c.g) && _valid(c.b) && _valid(c.a); }
+[[nodiscard]] constexpr bool _valid(const Fcolor& c) noexcept { return _valid(c.r) && _valid(c.g) && _valid(c.b) && _valid(c.a); }

commit d15e059b01f2d2d65ff0214170b64a88c9a628b0

xrCore: fix broken CMemoryWriter and Fcolor::set(a, r, g, b)
G E N I U S

Pls rewrite more without proper testing, it's always fun to debug
such crap (NO IT'S NOT).

Fixes: be1bbde569a2 ("xrCore: strictly-typed X-Ray pt I")
Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrCore/FS.cpp
@@ -97,8 +97,8 @@ void CMemoryWriter::w(const void* ptr, gsl::index count)
 
 void CMemoryWriter::reserve(gsl::index count)
 {
-    data = static_cast<std::byte*>(xr_malloc(mem_size));
     mem_size = count;
+    data = static_cast<std::byte*>(xr_malloc(mem_size));
 }
 
 bool CMemoryWriter::save_to(LPCSTR fn) const

ogsr_engine/xrCore/_color.h
@@ -55,9 +55,9 @@ struct XR_TRIVIAL Fcolor
         constexpr f32 f{1.0f / 255.0f};
 
         a = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 24));
-        b = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 16));
+        r = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 16));
         g = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw >> 8));
-        r = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw));
+        b = f * gsl::narrow_cast<f32>(gsl::narrow_cast<u8>(dw));
 
         return *this;
     }