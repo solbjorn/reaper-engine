solbjorn/reaper-engine

commit fcea0f17a8e9ac330cd1e2634b6d78c55d4272ec

xrCDB: cache cforms
From all collision models that the engine builds (cform / object space,
HOM, portals, sound occlusion, sound environment, ...), only the cform
build takes really long time -- nearly 0.5 seconds on l01_escape.
This is due that level.cform usually contains > 1 mln nodes, plus each
triangle needs to be modified to include material parameters.
Cache built cforms to $app_data_root$/levels_cache/<level_name>.cform.
This includes both OpCoDe collision model and modified TRIs. For the
above level, the resulting file is ~68 Mb. Even though we need to
calculate hashes for: level.cform itself (20+ Mb), gamemtl.xr (300 Kb),
and the cache file itself (^ 68 Mb), IOW read nearly 100 Mb, this now
takes 50 msec instead of 500, thanks to xxHash and probably
xr_memcpy128() as we align everything in the cache file to 16-byte
boundary to be able to copy stuff quickly using AVX.

Credits for the original implementation go to OpenXRay.

Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrCDB/xrCDB.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 
 #include <Opcode.h>
+#include <xxhash.h>
 
 using namespace CDB;
 using namespace Opcode;
@@ -93,6 +94,201 @@ size_t MODEL::memory()
     const size_t V = verts_count * sizeof(Fvector);
     const size_t T = tris_count * sizeof(TRI);
     return tree->GetUsedBytes() + V + T + sizeof(*this) + sizeof(*tree);
+}
+
+struct alignas(16) model_mid_hdr
+{
+    u64 tris_count;
+    u64 verts_count;
+    XXH64_hash_t tris_xxh;
+    XXH64_hash_t verts_xxh;
+};
+
+struct alignas(16) model_end_hdr
+{
+    u32 model_code;
+    u32 nodes_num;
+    u64 nodes;
+    XXH64_hash_t nodes_xxh;
+    u64 pad;
+};
+
+void MODEL::serialize_tree(IWriter* stream) const
+{
+    const u32 nodes_num = tree->GetNbNodes();
+    const auto* root = ((const AABBNoLeafTree*)tree->GetTree())->GetNodes();
+    const size_t size = roundup(nodes_num * sizeof(AABBNoLeafNode), 16);
+
+    const model_end_hdr hdr = {
+        .model_code = tree->GetModelCode(),
+        .nodes_num = nodes_num,
+        .nodes = (uintptr_t)root,
+        .nodes_xxh = XXH3_64bits(root, size),
+        .pad = 0,
+    };
+
+    stream->w(&hdr, sizeof(hdr));
+    stream->w(root, size);
+}
+
+bool MODEL::serialize(const char* file, u64 xxh, serialize_callback callback) const
+{
+    IWriter* wstream = FS.w_open(file);
+    if (!wstream)
+        return false;
+
+    if (callback)
+        callback(*wstream);
+
+    wstream->w_u64(xxh);
+    wstream->seek(roundup(wstream->tell(), 16));
+
+    const size_t trs = sizeof(TRI) * tris_count;
+    const size_t vrs = roundup(sizeof(Fvector) * verts_count, 16);
+
+    const model_mid_hdr hdr = {
+        .tris_count = tris_count,
+        .verts_count = verts_count,
+        .tris_xxh = XXH3_64bits(tris, trs),
+        .verts_xxh = XXH3_64bits(verts, vrs),
+    };
+
+    wstream->w(&hdr, sizeof(hdr));
+    wstream->w(tris, trs);
+    wstream->w(verts, vrs);
+
+    if (tree)
+        serialize_tree(wstream);
+
+    FS.w_close(wstream);
+
+    return true;
+}
+
+bool MODEL::deserialize_tree(IReader* stream)
+{
+    model_end_hdr hdr;
+    xr_memcpy128(&hdr, stream->pointer(), sizeof(hdr));
+    stream->advance(sizeof(hdr));
+
+    auto* mTree = xr_new<AABBNoLeafTree>();
+    auto* ptr = xr_alloc<AABBNoLeafNode>(hdr.nodes_num);
+    R_ASSERT(mTree && ptr);
+
+    const size_t size = hdr.nodes_num * sizeof(AABBNoLeafNode);
+    xr_memcpy128(ptr, stream->pointer(), size);
+
+    if (XXH3_64bits(ptr, size) != hdr.nodes_xxh)
+        goto err;
+
+    for (u32 i = 0; i < hdr.nodes_num; i++)
+    {
+        if (!ptr[i].HasPosLeaf())
+        {
+            ptr[i].mPosData -= hdr.nodes;
+            ptr[i].mPosData += (uintptr_t)ptr;
+        }
+        if (!ptr[i].HasNegLeaf())
+        {
+            ptr[i].mNegData -= hdr.nodes;
+            ptr[i].mNegData += (uintptr_t)ptr;
+        }
+    }
+
+    struct faketree
+    {
+        u8 pad1[8];
+        u32 mNbNodes;
+        u8 pad2[4];
+        AABBNoLeafNode* mNodes;
+    }* ft = reinterpret_cast<faketree*>(mTree);
+
+    ft->mNbNodes = hdr.nodes_num;
+    ft->mNodes = ptr;
+
+    struct fakemodel
+    {
+        u8 pad1[16];
+        u32 mModelCode;
+        u8 pad2[12];
+        AABBOptimizedTree* mTree;
+    }* fm = reinterpret_cast<fakemodel*>(tree);
+
+    fm->mModelCode = hdr.model_code;
+    fm->mTree = mTree;
+
+    return true;
+
+err:
+    xr_free(ptr);
+    xr_delete(mTree);
+
+    return false;
+}
+
+bool MODEL::deserialize(const char* file, u64 xxh, deserialize_callback callback)
+{
+    IReader* rstream = FS.r_open(file);
+    if (!rstream)
+        return false;
+
+    if (callback && !callback(*rstream))
+        goto err_close;
+
+    if (rstream->r_u64() != xxh)
+        goto err_close;
+
+    rstream->seek(roundup(rstream->tell(), 16));
+
+    xr_free(tris);
+    xr_free(verts);
+    xr_delete(tree);
+
+    model_mid_hdr mid;
+    xr_memcpy128(&mid, rstream->pointer(), sizeof(mid));
+    rstream->advance(sizeof(mid));
+
+    tris_count = mid.tris_count;
+    verts_count = mid.verts_count;
+
+    tris = xr_alloc<TRI>(tris_count);
+    const size_t trisSize = tris_count * sizeof(TRI);
+    xr_memcpy128(tris, rstream->pointer(), trisSize);
+    rstream->advance(trisSize);
+
+    if (XXH3_64bits(tris, trisSize) != mid.tris_xxh)
+        goto err_tris;
+
+    verts = xr_alloc<Fvector>(verts_count);
+    const size_t vertsSize = roundup(verts_count * sizeof(Fvector), 16);
+    xr_memcpy128(verts, rstream->pointer(), vertsSize);
+    rstream->advance(vertsSize);
+
+    if (XXH3_64bits(verts, vertsSize) != mid.verts_xxh)
+        goto err_verts;
+
+    if (rstream->eof())
+        return true;
+
+    tree = xr_new<Model>();
+    if (!deserialize_tree(rstream))
+        goto err_tree;
+
+    FS.r_close(rstream);
+    status = S_READY;
+
+    return true;
+
+err_tree:
+    xr_delete(tree);
+err_verts:
+    xr_free(verts);
+err_tris:
+    xr_free(tris);
+err_close:
+    FS.r_close(rstream);
+
+    return false;
 }
 
 COLLIDER::~COLLIDER() { r_free(); }

ogsr_engine/xrCDB/xrCDB.h
@@ -41,7 +41,9 @@ public:
 static_assert(sizeof(TRI) == 16);
 
 // Build callback
-typedef void __stdcall build_callback(Fvector* V, int Vcnt, TRI* T, int Tcnt, void* params);
+using build_callback = void(Fvector* V, size_t Vcnt, TRI* T, size_t Tcnt, void* params);
+using serialize_callback = void(IWriter& writer);
+using deserialize_callback = bool(IReader& reader);
 
 class Noncopyable
 {
@@ -98,6 +100,13 @@ public:
     void build_internal(Fvector* V, size_t Vcnt, TRI* T, size_t Tcnt, build_callback* bc = nullptr, void* bcp = nullptr);
     void build(Fvector* V, size_t Vcnt, TRI* T, size_t Tcnt, build_callback* bc = nullptr, void* bcp = nullptr);
     size_t memory();
+
+    bool serialize(const char* file, u64 xxh, serialize_callback callback = nullptr) const;
+    bool deserialize(const char* file, u64 xxh, deserialize_callback callback = nullptr);
+
+private:
+    void serialize_tree(IWriter* stream) const;
+    bool deserialize_tree(IReader* stream);
 };
 
 // Collider result

ogsr_engine/xrCDB/xrCDB.vcxproj
@@ -87,7 +87,7 @@
       <OmitFramePointers>false</OmitFramePointers>
       <EnableFiberSafeOptimizations>true</EnableFiberSafeOptimizations>
       <WholeProgramOptimization>false</WholeProgramOptimization>
-      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;$(SolutionDir)\3rd_party\Src\xxHash\xxHash;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PreprocessorDefinitions>WIN32;NDEBUG;_WINDOWS;_USRDLL;ICE_NO_DLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <StringPooling>true</StringPooling>
       <ExceptionHandling>
@@ -148,7 +148,7 @@
       <OmitFramePointers>false</OmitFramePointers>
       <EnableFiberSafeOptimizations>false</EnableFiberSafeOptimizations>
       <WholeProgramOptimization>false</WholeProgramOptimization>
-      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;$(SolutionDir)\3rd_party\Src\xxHash\xxHash;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PreprocessorDefinitions>WIN32;_WINDOWS;_USRDLL;ICE_NO_DLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <StringPooling>false</StringPooling>
       <ExceptionHandling>
@@ -212,7 +212,7 @@
       <OmitFramePointers>false</OmitFramePointers>
       <EnableFiberSafeOptimizations>true</EnableFiberSafeOptimizations>
       <WholeProgramOptimization>true</WholeProgramOptimization>
-      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;$(SolutionDir)\3rd_party\Src\xxHash\xxHash;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PreprocessorDefinitions>NDEBUG;_WINDOWS;_USRDLL;ICE_NO_DLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <StringPooling>true</StringPooling>
       <ExceptionHandling>
@@ -272,7 +272,7 @@
       <OmitFramePointers>false</OmitFramePointers>
       <EnableFiberSafeOptimizations>false</EnableFiberSafeOptimizations>
       <WholeProgramOptimization>false</WholeProgramOptimization>
-      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>$(SolutionDir)\3rd_party\Src\OpCoDe\OpCoDe;$(SolutionDir)\3rd_party\Src\xxHash\xxHash;..\xrCore;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PreprocessorDefinitions>_WINDOWS;_USRDLL;ICE_NO_DLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <StringPooling>false</StringPooling>
       <ExceptionHandling>

ogsr_engine/xrCDB/xr_area.cpp
@@ -6,6 +6,8 @@
 #include "../xr_3da/igame_level.h"
 #include "../xr_3da/xrLevel.h"
 
+#include <xxhash.h>
+
 using namespace collide;
 
 extern BOOL g_bLoaded;
@@ -175,7 +177,10 @@ int CObjectSpace::GetNearest(xr_vector<CObject*>& q_nearest, ICollisionForm* obj
 }
 
 //----------------------------------------------------------------------
-static void __stdcall build_callback(Fvector* V, int Vcnt, CDB::TRI* T, int Tcnt, void* params) { g_pGameLevel->Load_GameSpecific_CFORM(T, Tcnt); }
+static void __stdcall build_callback(Fvector* V, size_t Vcnt, CDB::TRI* T, size_t Tcnt, void* params) { g_pGameLevel->Load_GameSpecific_CFORM(T, Tcnt); }
+static void __stdcall serialize_callback(IWriter& writer) { g_pGameLevel->Load_GameSpecific_CFORM_Serialize(writer); }
+static bool __stdcall deserialize_callback(IReader& reader) { return g_pGameLevel->Load_GameSpecific_CFORM_Deserialize(reader); }
+
 void CObjectSpace::Load()
 {
     IReader* F = FS.r_open("$level$", "level.cform");
@@ -186,7 +191,21 @@ void CObjectSpace::Load()
     Fvector* verts = (Fvector*)F->pointer();
     CDB::TRI* tris = (CDB::TRI*)(verts + H.vertcount);
     R_ASSERT(CFORM_CURRENT_VERSION == H.version);
-    Static.build(verts, H.vertcount, tris, H.facecount, build_callback);
+
+    F->seek(0);
+    XXH64_hash_t xxh = XXH3_64bits(F->pointer(), F->length());
+
+    string_path cache;
+    xr_strcpy(cache, "levels_cache\\");
+    xr_strcat(cache, g_pGameLevel->name().c_str());
+    xr_strcat(cache, ".cform");
+    FS.update_path(cache, "$app_data_root$", cache);
+
+    if (!FS.exist(cache) || !Static.deserialize(cache, xxh, deserialize_callback))
+    {
+        Static.build(verts, H.vertcount, tris, H.facecount, build_callback);
+        Static.serialize(cache, xxh, serialize_callback);
+    }
 
     m_BoundingVolume.set(H.aabb);
     g_SpatialSpace->initialize(H.aabb);

ogsr_engine/xrCore/_std_extensions.h
@@ -140,6 +140,9 @@ IC s64 _max(s64 x, s64 y) { return x - ((x - y) & ((x - y) >> (sizeof(s64) * 8 -
 
 char* timestamp(string64& dest);
 
+// Round @val to next @mul boundary, where @mul is a power of two
+constexpr ICF size_t roundup(size_t val, long mul) { return (val + mul - 1) & -mul; }
+
 constexpr ICF void xr_memcpy_const(void* dst, const void* src, size_t size)
 {
     u8* cdst = (u8*)(dst);

ogsr_engine/xrGame/Level.h
@@ -195,7 +195,10 @@ public:
 
     virtual BOOL Load_GameSpecific_Before();
     virtual BOOL Load_GameSpecific_After();
-    virtual void Load_GameSpecific_CFORM(CDB::TRI* T, u32 count);
+
+    void Load_GameSpecific_CFORM_Serialize(IWriter& writer) override;
+    bool Load_GameSpecific_CFORM_Deserialize(IReader& reader) override;
+    void Load_GameSpecific_CFORM(CDB::TRI* T, size_t count) override;
 
     // Events
     virtual void OnEvent(EVENT E, u64 P1, u64 P2);
@@ -226,7 +229,7 @@ public:
     void ClientReceive();
     void ClientSend();
     void ClientSave();
-    
+
     virtual void Send(NET_Packet& P, u32 dwFlags = DPNSEND_GUARANTEED, u32 dwTimeout = 0);
 
     void g_cl_Spawn(LPCSTR name, u8 rp, u16 flags, Fvector pos); // only ask server
@@ -244,16 +247,16 @@ public:
     CLevel();
     virtual ~CLevel();
 
-    //названияе текущего уровня
+    // названияе текущего уровня
     virtual shared_str name() const;
 
     // gets the time from the game simulation
 
-    //возвращает время в милисекундах относительно начала игры
+    // возвращает время в милисекундах относительно начала игры
     ALife::_TIME_ID GetGameTime();
-    //возвращает время для энвайронмента в милисекундах относительно начала игры
+    // возвращает время для энвайронмента в милисекундах относительно начала игры
     ALife::_TIME_ID GetEnvironmentGameTime();
-    //игровое время в отформатированном виде
+    // игровое время в отформатированном виде
     void GetGameDateTime(u32& year, u32& month, u32& day, u32& hours, u32& mins, u32& secs, u32& milisecs);
 
     float GetGameTimeFactor();
@@ -284,7 +287,7 @@ public:
     CMapManager& MapManager() { return *m_map_manager; }
     //	CFogOfWarMngr&			FogOfWarMngr				()	{return *m_pFogOfWarMngr;}
 
-    //работа с пулями
+    // работа с пулями
 protected:
     CBulletManager* m_pBulletManager;
 
@@ -292,8 +295,8 @@ public:
     IC CBulletManager& BulletManager() { return *m_pBulletManager; }
 
     // by Mad Max
-    //bool IsServer();
-    //bool IsClient();
+    // bool IsServer();
+    // bool IsClient();
     CSE_Abstract* spawn_item(LPCSTR section, const Fvector& position, u32 level_vertex_id, u16 parent_id, bool return_item = false);
 
 protected:
@@ -368,8 +371,8 @@ IC CPHCommander& CLevel::ph_commander_scripts()
     return *m_ph_commander_scripts;
 }
 // by Mad Max
-//IC bool OnServer() { return Level().IsServer(); }
-//IC bool OnClient() { return Level().IsClient(); }
+// IC bool OnServer() { return Level().IsServer(); }
+// IC bool OnClient() { return Level().IsClient(); }
 
 class CPHWorld;
 extern CPHWorld* ph_world;
@@ -378,7 +381,7 @@ extern BOOL g_bDebugEvents;
 struct CKeyBinding
 {
 public:
-    CKeyBinding() { };
+    CKeyBinding() {};
 
     bool ignore = false;
 };

ogsr_engine/xrGame/Level_load.cpp
@@ -142,18 +142,21 @@ struct translation_pair
     IC bool operator<(const u16& id) const { return (m_id < id); }
 };
 
-void CLevel::Load_GameSpecific_CFORM(CDB::TRI* tris, u32 count)
+void CLevel::Load_GameSpecific_CFORM_Serialize(IWriter& writer) { writer.w_u64(GMLib.get_hash()); }
+bool CLevel::Load_GameSpecific_CFORM_Deserialize(IReader& reader) { return reader.r_u64() == GMLib.get_hash(); }
+
+void CLevel::Load_GameSpecific_CFORM(CDB::TRI* tris, size_t count)
 {
     typedef xr_vector<translation_pair> ID_INDEX_PAIRS;
     ID_INDEX_PAIRS translator;
     translator.reserve(GMLib.CountMaterial());
     u16 default_id = (u16)GMLib.GetMaterialIdx("default");
-    translator.push_back(translation_pair(u32(-1), default_id));
+    translator.emplace_back(u32(-1), default_id);
 
     u16 index = 0, static_mtl_count = 1;
     int max_ID = 0;
     int max_static_ID = 0;
-    for (GameMtlIt I = GMLib.FirstMaterial(); GMLib.LastMaterial() != I; ++I, ++index)
+    for (auto I = GMLib.FirstMaterial(); GMLib.LastMaterial() != I; ++I, ++index)
     {
         if (!(*I)->Flags.test(SGameMtl::flDynamic))
         {
@@ -174,11 +177,11 @@ void CLevel::Load_GameSpecific_CFORM(CDB::TRI* tris, u32 count)
         CDB::TRI* E = tris + count;
         for (; I != E; ++I)
         {
-            ID_INDEX_PAIRS::iterator i = std::find(translator.begin(), translator.end(), (u16)(*I).material);
+            const auto i = std::find(translator.cbegin(), translator.cend(), (u16)(*I).material);
             if (i != translator.end())
             {
                 (*I).material = (*i).m_index;
-                SGameMtl* mtl = GMLib.GetMaterialByIdx((*i).m_index);
+                const SGameMtl* mtl = GMLib.GetMaterialByIdx((*i).m_index);
                 (*I).suppress_shadows = mtl->Flags.is(SGameMtl::flSuppressShadows);
                 (*I).suppress_wm = mtl->Flags.is(SGameMtl::flSuppressWallmarks);
                 continue;
@@ -195,11 +198,11 @@ void CLevel::Load_GameSpecific_CFORM(CDB::TRI* tris, u32 count)
         CDB::TRI* E = tris + count;
         for (; I != E; ++I)
         {
-            ID_INDEX_PAIRS::iterator i = std::lower_bound(translator.begin(), translator.end(), (u16)(*I).material);
-            if ((i != translator.end()) && ((*i).m_id == (*I).material))
+            const auto i = std::lower_bound(translator.cbegin(), translator.cend(), (u16)(*I).material);
+            if ((i != translator.cend()) && ((*i).m_id == (*I).material))
             {
                 (*I).material = (*i).m_index;
-                SGameMtl* mtl = GMLib.GetMaterialByIdx((*i).m_index);
+                const SGameMtl* mtl = GMLib.GetMaterialByIdx((*i).m_index);
                 (*I).suppress_shadows = mtl->Flags.is(SGameMtl::flSuppressShadows);
                 (*I).suppress_wm = mtl->Flags.is(SGameMtl::flSuppressWallmarks);
                 continue;

ogsr_engine/xr_3da/GameMtlLib.cpp
@@ -1,9 +1,9 @@
 //---------------------------------------------------------------------------
 #include "stdafx.h"
 
-
 #include "GameMtlLib.h"
-//#include "../include/xrapi/xrapi.h"
+
+#include <xxhash.h>
 
 CGameMtlLibrary GMLib;
 // CSound_manager_interface*	Sound = NULL;
@@ -101,7 +101,7 @@ void CGameMtlLibrary::Load()
         OBJ->close();
     }
 
-    //const u32 actor_material_idx = GetMaterialID(pSettings->r_string("actor", "material"));
+    // const u32 actor_material_idx = GetMaterialID(pSettings->r_string("actor", "material"));
 
     OBJ = F->open_chunk(GAMEMTLS_CHUNK_MTLS_PAIR);
     if (OBJ)
@@ -156,6 +156,21 @@ void CGameMtlLibrary::Load()
         }
     */
     FS.r_close(F);
+}
+
+u64 CGameMtlLibrary::get_hash()
+{
+    XXH64_hash_t xxh = XXH64_hash_t(-1);
+    string_path name;
+
+    if (!FS.exist(name, _game_data_, GAMEMTL_FILENAME))
+        return xxh;
+
+    IReader* r = FS.r_open(name);
+    xxh = XXH3_64bits(r->pointer(), r->length());
+    FS.r_close(r);
+
+    return xxh;
 }
 
 #ifdef GM_NON_GAME

commit f22d86fc5bb60acfb477b5594bb05e757cd543fc

xrCDB: fix a rare exit-time crash
OpCoDe allocates nodes using `new[]`, not malloc(), and deletes it
via `delete[]` obviously. My choice of xr_alloc() led to alloc-free
imbalance, which started to show up only recently for some reason
(mimalloc improvements?).
Use std::unique_ptr<[]> + ::release(). Unfortunately, this leads to
that the array of nodes is not 16-byte aligned anymore, so we can't
use xr_memcpy128() anymore. Just switch to plain std::memcpy(), I
don't think this will increase level loading time significantly.

Fixes: fcea0f17a8e9 ("xrCDB: cache cforms")
Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

ogsr_engine/xrCDB/xrCDB.cpp
@@ -10,6 +10,7 @@ XR_DIAG_IGNORE("-Wfloat-conversion");
 XR_DIAG_IGNORE("-Wfloat-equal");
 XR_DIAG_IGNORE("-Wheader-hygiene");
 XR_DIAG_IGNORE("-Wold-style-cast");
+XR_DIAG_IGNORE("-Woverloaded-virtual");
 XR_DIAG_IGNORE("-Wshorten-64-to-32");
 XR_DIAG_IGNORE("-Wsign-conversion");
 XR_DIAG_IGNORE("-Wunknown-pragmas");
@@ -116,6 +117,8 @@ gsl::index MODEL::memory() const
     return tree->GetUsedBytes() + V + T + gsl::index{sizeof(*this)} + gsl::index{sizeof(*tree)};
 }
 
+namespace
+{
 struct alignas(16) model_mid_hdr
 {
     s64 tris_count;
@@ -132,15 +135,16 @@ struct alignas(16) model_end_hdr
     xxh::XXH64_hash_t nodes_xxh;
     u64 pad;
 };
+} // namespace
 
 void MODEL::serialize_tree(IWriter* stream) const
 {
     // This should be smart_cast<>()/dynamic_cast<>(), but OpCoDe doesn't use our custom RTTI.
     // So we just rely on that `AABBOptimizedTree` starts at offset 0 inside `AABBNoLeafTree`.
     // OpCoDe itself uses C-style casts for downcasting, which is roughly the same.
-    const auto* root = reinterpret_cast<const AABBNoLeafTree*>(tree->GetTree())->GetNodes();
+    const auto root = reinterpret_cast<const AABBNoLeafTree*>(tree->GetTree())->GetNodes();
     const auto nodes_num = tree->GetNbNodes();
-    const auto size = xr::roundup(nodes_num * sizeof(AABBNoLeafNode), 16uz);
+    const auto size = nodes_num * sizeof(AABBNoLeafNode);
 
     const model_end_hdr hdr = {
         .model_code = tree->GetModelCode(),
@@ -194,33 +198,29 @@ bool MODEL::deserialize_tree(IReader* stream)
     xr_memcpy128(&hdr, stream->pointer(), sizeof(hdr));
     stream->advance(sizeof(hdr));
 
-    auto* mTree = xr_new<AABBNoLeafTree>();
-    auto* ptr = xr_alloc<AABBNoLeafNode>(hdr.nodes_num);
-
+    auto nodes = std::make_unique_for_overwrite<AABBNoLeafNode[]>(hdr.nodes_num);
     const auto size = hdr.nodes_num * sizeof(AABBNoLeafNode);
-    xr_memcpy128(ptr, stream->pointer(), size);
+    std::memcpy(static_cast<void*>(&nodes[0]), stream->pointer(), size);
 
-    if (xxh::XXH3_64bits(ptr, size) != hdr.nodes_xxh)
-    {
-        xr_free(ptr);
-        xr_delete(mTree);
-
+    if (xxh::XXH3_64bits(&nodes[0], size) != hdr.nodes_xxh)
         return false;
+
+    for (auto& node : std::span{&nodes[0], hdr.nodes_num})
+    {
+        if (!node.HasPosLeaf())
+        {
+            node.mPosData -= hdr.nodes;
+            node.mPosData += reinterpret_cast<uintptr_t>(&nodes[0]);
+        }
+
+        if (!node.HasNegLeaf())
+        {
+            node.mNegData -= hdr.nodes;
+            node.mNegData += reinterpret_cast<uintptr_t>(&nodes[0]);
+        }
     }
 
-    for (gsl::index i{}; i < hdr.nodes_num; ++i)
-    {
-        if (!ptr[i].HasPosLeaf())
-        {
-            ptr[i].mPosData -= hdr.nodes;
-            ptr[i].mPosData += reinterpret_cast<uintptr_t>(ptr);
-        }
-        if (!ptr[i].HasNegLeaf())
-        {
-            ptr[i].mNegData -= hdr.nodes;
-            ptr[i].mNegData += reinterpret_cast<uintptr_t>(ptr);
-        }
-    }
+    auto mTree = std::make_unique<AABBNoLeafTree>();
 
     struct faketree
     {
@@ -228,10 +228,10 @@ bool MODEL::deserialize_tree(IReader* stream)
         u32 mNbNodes;
         u8 pad2[4];
         AABBNoLeafNode* mNodes;
-    }* ft = reinterpret_cast<faketree*>(mTree);
+    }* ft = reinterpret_cast<faketree*>(mTree.get());
 
     ft->mNbNodes = hdr.nodes_num;
-    ft->mNodes = ptr;
+    ft->mNodes = nodes.release();
 
     struct fakemodel
     {
@@ -242,7 +242,7 @@ bool MODEL::deserialize_tree(IReader* stream)
     }* fm = reinterpret_cast<fakemodel*>(tree);
 
     fm->mModelCode = hdr.model_code;
-    fm->mTree = mTree;
+    fm->mTree = mTree.release();
 
     return true;
 }

ogsr_engine/xrCDB/xrCDB_box.cpp
@@ -7,6 +7,7 @@ XR_DIAG_IGNORE("-Wfloat-conversion");
 XR_DIAG_IGNORE("-Wfloat-equal");
 XR_DIAG_IGNORE("-Wheader-hygiene");
 XR_DIAG_IGNORE("-Wold-style-cast");
+XR_DIAG_IGNORE("-Woverloaded-virtual");
 XR_DIAG_IGNORE("-Wshorten-64-to-32");
 XR_DIAG_IGNORE("-Wsign-conversion");
 XR_DIAG_IGNORE("-Wunknown-pragmas");

ogsr_engine/xrCDB/xrCDB_frustum.cpp
@@ -7,6 +7,7 @@ XR_DIAG_IGNORE("-Wfloat-conversion");
 XR_DIAG_IGNORE("-Wfloat-equal");
 XR_DIAG_IGNORE("-Wheader-hygiene");
 XR_DIAG_IGNORE("-Wold-style-cast");
+XR_DIAG_IGNORE("-Woverloaded-virtual");
 XR_DIAG_IGNORE("-Wshorten-64-to-32");
 XR_DIAG_IGNORE("-Wsign-conversion");
 XR_DIAG_IGNORE("-Wunknown-pragmas");

ogsr_engine/xrCDB/xrCDB_ray.cpp
@@ -9,6 +9,7 @@ XR_DIAG_IGNORE("-Wfloat-conversion");
 XR_DIAG_IGNORE("-Wfloat-equal");
 XR_DIAG_IGNORE("-Wheader-hygiene");
 XR_DIAG_IGNORE("-Wold-style-cast");
+XR_DIAG_IGNORE("-Woverloaded-virtual");
 XR_DIAG_IGNORE("-Wshorten-64-to-32");
 XR_DIAG_IGNORE("-Wsign-conversion");
 XR_DIAG_IGNORE("-Wunknown-pragmas");