bbc5bb7ff8023cf825bdcf5b89e59ebc8998bdd0
Alexander Lobakin <alobakin@mailbox.org>
2026-07-26 20:21:10 +0200

    chore(build): compile with more friendly CPU instruction set
    Another good hint from Arnie: `-march=skylake` is a good instruction
    set, but using it can hurt even more modern Intel CPUs like my
    Alder Lake, not speaking of AMDs.
    
    Pick completely vendor-agnostic `-march=x86-64-v3`, which implies
    most of Skylake's subset, definitely everything most important
    for us compile-time, but performs equally good on a Skylake,
    an Alder Lake, or an AMD. It still sets the same bottom line of
    2013-2015 meaning the binaries won't work on older CPUs which
    we don't care about. And yes, this option is better than bare
    `-mavx2`.
    
    Also enable runtime dispatching in zlib-ng, which I disabled by
    mistake, to allow running more optimized code on newer CPUs
    (AVX-VNNI on Alder Lake and so on). The rest of libraries
    looks fine, except OpenJPH which currently doesn't compile
    with runtime dispatching.
    
    Assisted-by: Cyberdyne Systems Series 800 Model 101 Version 2.4
    Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

--- 3rd_party/Src/3rd_party_properties.cmake
+++ 3rd_party/Src/3rd_party_properties.cmake
@@ -8,7 +8,7 @@ endif()
 
 set(conformance_options "/Brepro /bigobj /permissive- /volatile:iso /Zc:inline /Zc:preprocessor /Zc:enumTypes /Zc:lambda /Zc:__STDC__ /Zc:__cplusplus /Zc:externConstexpr /Zc:throwingNew /Zc:checkGwOdr /Zc:forScope /Zc:templateScope /Zc:u8EscapeEncoding /Zc:wchar_t /JMC- /EHsc- /DNOMINMAX /DSTRICT -fstrict-aliasing -fno-delayed-template-parsing -fno-wrapv /D_CRT_STDIO_ISO_WIDE_SPECIFIERS -fno-ms-compatibility -fgnuc-version=0 /utf-8")
 
-set(llvm_options "-fuse-ld=lld-link -gcodeview-ghash -march=skylake -mavx2 -mbmi -mbmi2 -mfma -mlzcnt -mvpclmulqdq -flto -fmerge-all-constants -fforce-emit-vtables -fwhole-program-vtables /clang:-fcoro-aligned-allocation")
+set(llvm_options "-fuse-ld=lld-link -gcodeview-ghash -march=x86-64-v3 -mavx2 -mbmi -mbmi2 -mfma -mlzcnt -flto -fmerge-all-constants -fforce-emit-vtables -fwhole-program-vtables /clang:-fcoro-aligned-allocation")
 
 set(warning_options "-Wextra -Wmost -Wno-error=unused-command-line-argument -Werror=format -Wformat-nonliteral -Werror=format-pedantic -Werror=format-signedness -Werror=format-type-confusion -Werror=inconsistent-missing-override -Werror=microsoft -Werror=move -Werror=nan-infinity-disabled -Werror=parentheses -Werror=strict-aliasing -Werror=tautological-compare -Werror=typename-missing -Werror=weak-vtables")
 
